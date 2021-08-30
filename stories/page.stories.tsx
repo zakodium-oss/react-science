8465ea52e8a97d95ef51f3258cd56a902216789d
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-08-30 16:58:21 +0200

    chore: install storybook

---
+++ stories/page.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Meta } from "@storybook/react";
+
+export default {
+  title: "Default",
+} as Meta;
+
+export function Control() {
+  return <p>Hello, World!</p>;
+}
