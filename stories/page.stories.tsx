ed594794878ab05382d9fbb615a0bc2843d0d333
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-08-31 11:08:46 +0200

    feat: add normalized css & import on vite & sb

--- stories/page.stories.tsx
+++ stories/page.stories.tsx
@@ -2,11 +2,17 @@ import { Meta } from '@storybook/react';
 import React from 'react';
 
 import App from '../src/App';
+import '../public/normalize.css';
 
 export default {
   title: 'Default',
 } as Meta;
 
 export function Control() {
-  return <App />;
+  return (
+    <>
+      <App />
+      <button type="button">Normalized button</button>
+    </>
+  );
 }
