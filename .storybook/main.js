e3474c7891c14753fe8d30f591105a568498463f
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-08-31 10:15:46 +0200

    chore: install story viewer addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ["../stories/**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-storysource",
+  ],
 };
