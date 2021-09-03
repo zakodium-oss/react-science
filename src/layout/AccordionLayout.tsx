18b4d3ceb5bc3431ed33c2e9ecaeeabe3a71b0b0
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-09-03 15:43:33 +0200

    refactor: change emotion to use object

--- src/layout/AccordionLayout.tsx
+++ src/layout/AccordionLayout.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { css, SerializedStyles } from '@emotion/react';
+import { css } from '@emotion/react';
 import React, { ReactNode, useState } from 'react';
 
 interface AccordionLayoutProps {
@@ -11,57 +11,52 @@ interface AccordionLayoutProps {
 interface AccordionItemProps {
   title: string;
   children: ReactNode;
-  defaultOpended?: boolean;
+  defaultOpened?: boolean;
 }
 
-const styles: Record<
-  'container' | 'item' | 'header',
-  (object?: any) => SerializedStyles
-> = {
-  container: () => {
-    return css`
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-    `;
-  },
+const styles = {
+  container: css({
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  }),
   item: (isOpen: boolean) => {
-    return css`
-      display: flex;
-      flex-direction: column;
-      ${isOpen && 'flex: 1 1 0%;'}
-    `;
+    return css([
+      {
+        display: 'flex',
+        flexDirection: 'column',
+        transition: 'flex-grow 500ms',
+      },
+      isOpen && { flex: '1 1 0%' },
+    ]);
   },
-  header: () => {
-    return css`
-      cursor: pointer;
-      padding: 5px 12px;
-      display: flex;
-      align-items: center;
-      font-size: 11px;
-      border-color: rgb(213, 213, 213) currentcolor currentcolor;
-      border-style: solid none none;
-      border-width: 0.55px medium medium;
-      border-image: none 100% / 1 / 0 stretch;
-      color: rgb(45, 45, 45);
-      font-family: Arial;
-      font-weight: bold;
-      text-decoration: none;
-      text-shadow: rgb(255, 255, 255) 0px 1px 0px;
-      background: rgb(255, 255, 255)
-        linear-gradient(rgb(240, 240, 240) 5%, rgb(225, 225, 225) 100%) repeat
-        scroll 0% 0%;
-    `;
-  },
+  header: css({
+    cursor: 'pointer',
+    padding: '5px 12px',
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: '11px',
+    borderColor: 'rgb(213, 213, 213) currentcolor currentcolor',
+    borderStyle: 'solid none none',
+    borderWidth: '0.55px, medium medium',
+    borderImage: 'none 100% / 1 / 0 stretch',
+    color: 'rgb(45, 45, 45)',
+    fontFamily: 'Arial',
+    fontWeight: 'bold',
+    textDecoration: 'none',
+    textShadow: 'rgb(255, 255, 255) 0px 1px 0px',
+    background:
+      'rgb(255, 255, 255) linear-gradient(rgb(240, 240, 240) 5%, rgb(225, 225, 225) 100%) repeat scroll 0% 0%',
+  }),
 };
 
 export function AccordionLayout(props: AccordionLayoutProps) {
-  return <div css={styles.container()}>{props.children}</div>;
+  return <div css={styles.container}>{props.children}</div>;
 }
 
 AccordionLayout.Item = function AccordionItem(props: AccordionItemProps) {
-  const { children, title, defaultOpended = false } = props;
-  const [open, setOpen] = useState(defaultOpended);
+  const { children, title, defaultOpened = false } = props;
+  const [open, setOpen] = useState(defaultOpened);
 
   function handleClick() {
     setOpen(!open);
@@ -69,8 +64,8 @@ AccordionLayout.Item = function AccordionItem(props: AccordionItemProps) {
 
   return (
     <div onClick={handleClick} css={styles.item(open)}>
-      <div css={styles.header()}>{title}</div>
-      {open && <div>{children}</div>}
+      <div css={styles.header}>{title}</div>
+      <div style={{ display: open ? 'block' : 'none' }}>{children}</div>
     </div>
   );
 };
