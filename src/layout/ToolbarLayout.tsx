575ba7574d485ba062100a0791c2a4f0a8dfbae5
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-09-03 13:40:12 +0200

    fix: add right border on Toolbar

--- src/layout/ToolbarLayout.tsx
+++ src/layout/ToolbarLayout.tsx
@@ -34,7 +34,7 @@ const styles: Record<
         flex-direction: column;
         max-width: ${size};
         min-height: 100%;
-        border-right: '${border};
+        border-right: ${border};
       `;
     }
     return css`
