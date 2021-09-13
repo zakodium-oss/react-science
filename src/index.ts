9f2f3e238b8d676577f9b2d0fad9f39e739af73d
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-09-13 16:25:29 +0200

    feat: add Modal to components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Accordion } from './layout/Accordion';
 import { Header } from './layout/Header';
+import { Modal } from './layout/Modal';
 import { RootLayout } from './layout/RootLayout';
 import { SplitPane } from './layout/SplitPane';
 import { Toolbar } from './layout/Toolbar';
 
-export { Accordion, Header, RootLayout, SplitPane, Toolbar };
+export { Accordion, Header, Modal, RootLayout, SplitPane, Toolbar };
