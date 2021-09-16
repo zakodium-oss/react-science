55d5983a1715f2f1b5419bf6cbfb147cfab277d5
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-09-16 11:57:04 +0200

    test: add test on Split & Root

--- src/layout/__tests__/rootLayout.test.tsx
+++ src/layout/__tests__/rootLayout.test.tsx
@@ -6,19 +6,19 @@ import { RootLayout } from '../RootLayout';
 beforeEach(() => {
   render(
     <RootLayout style={{ backgroundColor: 'red' }}>
-      <p>Hello, World!</p>
+      <p>A</p>
     </RootLayout>,
   );
 });
 
 describe('<RootLayout />', () => {
   it('display the RootLayout', () => {
-    const element = screen.getByText('Hello, World!');
+    const element = screen.getByText('A');
     expect(element).toBeInTheDocument();
   });
 
   it('add the style if you give the props style', () => {
-    const element = screen.getByText('Hello, World!');
+    const element = screen.getByText('A');
     expect(element.parentElement).toHaveStyle('background-color: red');
   });
 });
