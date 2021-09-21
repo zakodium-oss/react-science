911d3525708a970ddb9be1b83e83266354624bfb
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-09-21 13:33:21 +0200

    refactor: import / export hooks from src

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,22 @@ import { Modal } from './layout/Modal';
 import { RootLayout } from './layout/RootLayout';
 import { SplitPane } from './layout/SplitPane';
 import { Toolbar } from './layout/Toolbar';
+import {
+  useAccordionContext,
+  useToggleAccordion,
+} from './layout/context/AccordionContext';
+import { useSplitPaneSize } from './layout/hooks/useSplitPaneSize';
+import { useToggle } from './layout/hooks/useToggle';
 
-export { Accordion, Header, Modal, RootLayout, SplitPane, Toolbar };
+export {
+  Accordion,
+  Header,
+  Modal,
+  RootLayout,
+  SplitPane,
+  Toolbar,
+  useSplitPaneSize,
+  useAccordionContext,
+  useToggle,
+  useToggleAccordion,
+};
