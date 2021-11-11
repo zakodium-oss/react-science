8d4f27c448f6f528c4c77e67a5b89ad574eb7499
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2021-11-11 21:53:53 +0100

    fix issus

--- src/layout/hooks/useSplitPaneSize.tsx
+++ src/layout/hooks/useSplitPaneSize.tsx
@@ -32,30 +32,28 @@ export function useSplitPaneSize(options: HookOptions) {
   const mouseDownCallback = useCallback(() => {
     function onMouseMove(event: MouseEvent) {
       if (mouseRef.current.moving) {
+        let changed = true;
         const { clientX, clientY } = event;
-
         const movementX = clientX - mouseRef.current.x;
         const movementY = clientY - mouseRef.current.y;
-
         if (type === 'px') {
           setSize(([currentSize]) => {
             if (parentRef.current) {
-              return [
-                getValueFromSplitter(
-                  sideSeparation,
-                  orientation === 'horizontal' ? movementX : movementY,
-                  currentSize,
-                  type,
-                  {
-                    min: 50,
-                    max:
-                      orientation === 'horizontal'
-                        ? parentRef.current.clientWidth - 50
-                        : parentRef.current.clientHeight - 50,
-                  },
-                ),
+              const newSize = getValueFromSplitter(
+                sideSeparation,
+                orientation === 'horizontal' ? movementX : movementY,
+                currentSize,
                 type,
-              ];
+                {
+                  min: 50,
+                  max:
+                    orientation === 'horizontal'
+                      ? parentRef.current.clientWidth - 50
+                      : parentRef.current.clientHeight - 50,
+                },
+              );
+              if (newSize === currentSize) changed = false;
+              return [newSize, type];
             }
 
             return [currentSize, type];
@@ -68,29 +66,28 @@ export function useSplitPaneSize(options: HookOptions) {
                   (movementX / parentRef.current?.clientWidth) * 100;
                 const diffY =
                   (movementY / parentRef.current?.clientHeight) * 100;
-
-                return [
-                  getValueFromSplitter(
-                    sideSeparation,
-                    orientation === 'horizontal' ? diffX : diffY,
-                    currentSize,
-                    type,
-                    { min: 5, max: 95 },
-                  ),
+                let newSize = getValueFromSplitter(
+                  sideSeparation,
+                  orientation === 'horizontal' ? diffX : diffY,
+                  currentSize,
                   type,
-                ];
+                  { min: 5, max: 95 },
+                );
+                if (newSize === currentSize) changed = false;
+                return [newSize, type];
               }
 
               return [currentSize, type];
             });
           }
         }
-
-        mouseRef.current = {
-          moving: true,
-          x: clientX,
-          y: clientY,
-        };
+        if (changed) {
+          mouseRef.current = {
+            moving: true,
+            x: clientX,
+            y: clientY,
+          };
+        }
       }
 
       onChange(`${size}${type}` as InitialSeperation);
