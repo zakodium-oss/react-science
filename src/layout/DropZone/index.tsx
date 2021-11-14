f560f57507287ea0098ebc8d8bae8de3e4271f91
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2021-11-14 17:07:16 +0100

    fix import style.css

--- src/layout/DropZone/index.tsx
+++ src/layout/DropZone/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import 'style.css';
+import './style.css';
 
 import uploadImg from './upload.png';
 
