b973aa72e81b0b2189c1a25cbec20051809e8f5e
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2021-11-15 21:03:07 +0100

    add axis

--- src/layout/PlotXY.tsx
+++ src/layout/PlotXY.tsx
@@ -1,23 +1,28 @@
 import * as d3 from 'd3';
-import React, {
-  useEffect,
-  useState,
-  useRef,
-  Dispatch,
-  SetStateAction,
-} from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function LineChart() {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const data = [
+    65, 50, 40, 90, 15, 20, 8, 90, 65, 50, 40, 90, 15, 20, 8, 90, 65, 90, 15,
+    20, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90,
+    65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90,
+    65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90,
+    65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90,
+    65, 8, 90, 65, 8, 90, 70, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90,
+    65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65, 8, 90, 65,
+  ];
   const svgRef = useRef<SVGSVGElement>(null);
   useEffect(() => {
-    const data = [65, 50, 40, 90, 15, 20, 8, 90, 65];
-    const w = 400;
+    const w = 700;
     const h = 100;
     const svg = d3
       .select(svgRef.current)
       .attr('width', w)
       .attr('height', h)
-      .style('background', '#d3d3d3');
+      .style('background', '#d3d3d3')
+      .style('overflow', 'visible')
+      .style('margin', '50px');
 
     const xScale = d3
       .scaleLinear()
@@ -25,11 +30,17 @@ function LineChart() {
       .range([0, w]);
     const yScale = d3.scaleLinear().domain([0, h]).range([h, 0]);
     const generateScaledLine = d3
-      .line<number[]>()
+      .line()
       .x((d, i) => xScale(i))
       .y(yScale)
       .curve(d3.curveCardinal);
-
+    const xAxis = d3
+      .axisBottom(xScale)
+      .ticks(data.length / 10)
+      .tickFormat((i) => i);
+    const yAxis = d3.axisLeft(yScale).ticks(5);
+    svg.append('g').call(xAxis).attr('transform', `translate(0,${h})`);
+    svg.append('g').call(yAxis);
     svg
       .selectAll('.line')
       .data([data])
