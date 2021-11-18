0b53879c95b73181f88822c9aae1f8e96cd719ea
Wadjih Bencheikh <56769876+wadjih-bencheikh18@users.noreply.github.com>
2021-11-18 10:48:14 +0100

    Fix SplitPane movement (#80)
    * update packages
    
    * fix splitPane (stop when go out the box)
    
    * fix with perttier
    
    * fix packages
    
    * remove the changements
    
    * fix issus
    
    * reset package and package lock
    
    * fix the small issus
    
    * reset last commit
    
    * fix the problem + add comments

--- src/layout/hooks/useSplitPaneSize.tsx
+++ src/layout/hooks/useSplitPaneSize.tsx
@@ -33,29 +33,48 @@ export function useSplitPaneSize(options: HookOptions) {
     function onMouseMove(event: MouseEvent) {
       if (mouseRef.current.moving) {
         const { clientX, clientY } = event;
-
         const movementX = clientX - mouseRef.current.x;
         const movementY = clientY - mouseRef.current.y;
-
+        const movement = orientation === 'horizontal' ? movementX : movementY; //the movement expected
         if (type === 'px') {
           setSize(([currentSize]) => {
             if (parentRef.current) {
-              return [
-                getValueFromSplitter(
-                  sideSeparation,
-                  orientation === 'horizontal' ? movementX : movementY,
-                  currentSize,
-                  type,
-                  {
-                    min: 50,
-                    max:
-                      orientation === 'horizontal'
-                        ? parentRef.current.clientWidth - 50
-                        : parentRef.current.clientHeight - 50,
-                  },
-                ),
+              const newSize = getValueFromSplitter(
+                sideSeparation,
+                movement,
+                currentSize,
                 type,
-              ];
+                {
+                  min: 50,
+                  max:
+                    orientation === 'horizontal'
+                      ? parentRef.current.clientWidth - 50
+                      : parentRef.current.clientHeight - 50,
+                },
+              );
+              if (newSize !== currentSize) {
+                let movement1; //the real movement
+                if (movement * (newSize - currentSize) > 0) {
+                  //check that movement and movement1 have the same sign
+                  movement1 = newSize - currentSize;
+                } else {
+                  movement1 = currentSize - newSize;
+                }
+                mouseRef.current = {
+                  //set x y of the separation
+                  moving: true,
+                  x:
+                    orientation === 'horizontal'
+                      ? mouseRef.current.x + movement1
+                      : mouseRef.current.x,
+                  y:
+                    orientation === 'horizontal'
+                      ? mouseRef.current.y
+                      : mouseRef.current.y + movement1,
+                };
+              }
+
+              return [newSize, type];
             }
 
             return [currentSize, type];
@@ -64,33 +83,52 @@ export function useSplitPaneSize(options: HookOptions) {
           if (parentRef.current) {
             setSize(([currentSize]) => {
               if (parentRef.current) {
-                const diffX =
-                  (movementX / parentRef.current?.clientWidth) * 100;
+                const diffX = (movementX / parentRef.current.clientWidth) * 100;
                 const diffY =
-                  (movementY / parentRef.current?.clientHeight) * 100;
-
-                return [
-                  getValueFromSplitter(
-                    sideSeparation,
-                    orientation === 'horizontal' ? diffX : diffY,
-                    currentSize,
-                    type,
-                    { min: 5, max: 95 },
-                  ),
+                  (movementY / parentRef.current.clientHeight) * 100;
+                const diff = orientation === 'horizontal' ? diffX : diffY; //the diffrance expected
+                let newSize = getValueFromSplitter(
+                  sideSeparation,
+                  diff,
+                  currentSize,
                   type,
-                ];
+                  { min: 5, max: 95 },
+                );
+                if (newSize !== currentSize) {
+                  let diff1; //the real diffrence
+                  if (diff * (newSize - currentSize) > 0) {
+                    //check that diff and diff1 have the same sign
+                    diff1 =
+                      Math.round(
+                        (newSize - currentSize + Number.EPSILON) * 100,
+                      ) / 100;
+                  } else {
+                    diff1 =
+                      Math.round(
+                        (currentSize - newSize + Number.EPSILON) * 100,
+                      ) / 100;
+                  }
+                  mouseRef.current = {
+                    moving: true,
+                    x:
+                      orientation === 'horizontal'
+                        ? mouseRef.current.x +
+                          (diff1 * parentRef.current.clientWidth) / 100
+                        : mouseRef.current.x,
+                    y:
+                      orientation === 'horizontal'
+                        ? mouseRef.current.y
+                        : mouseRef.current.y +
+                          (diff1 * parentRef.current.clientHeight) / 100,
+                  };
+                }
+                return [newSize, type];
               }
 
               return [currentSize, type];
             });
           }
         }
-
-        mouseRef.current = {
-          moving: true,
-          x: clientX,
-          y: clientY,
-        };
       }
 
       onChange(`${size}${type}` as InitialSeperation);
@@ -115,7 +153,6 @@ export function useSplitPaneSize(options: HookOptions) {
         x: event.clientX,
         y: event.clientY,
       };
-
       mouseDownCallback();
     },
   };
@@ -129,7 +166,6 @@ function getValueFromSplitter(
   options: { min: number; max: number },
 ) {
   let val = 0;
-
   if (position === 'end') {
     val = currentSize - value;
   } else {
