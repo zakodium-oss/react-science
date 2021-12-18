b633ef31677690a1d55a226ccdd634080e76ad22
fname lname <jw_bencheikh@esi.dz>
2021-12-18 19:59:58 +0100

    docs: improve plotxy story

--- stories/plotxy.stories.tsx
+++ stories/plotxy.stories.tsx
@@ -14,44 +14,6 @@ export default {
   title: 'Layout/PlotXY',
 } as Meta;
 
-/**
-export function TestPlot() {
-  return (
-    <Plot
-      height={540}
-      width={900}
-      margin={{ bottom: 50, left: 100, top: 50, right: 10 }}
-      seriesViewportStyle={{
-        stroke: 'black',
-        strokeWidth: 0.3,
-      }}
-    >
-      <Heading title="COVID-19 cases in USA (2020)" />
-      <LineSeries
-        data={data}
-        xAxis="x"
-        yAxis="y"
-        lineStyle={{ stroke: 'red', strokeWidth: 2 }}
-      />
-      <Axis
-        id="x"
-        position="bottom"
-        label="Week"
-        labelSpace={30}
-        paddingStart={0}
-        paddingEnd={0}
-      />
-      <Axis
-        id="y"
-        position="left"
-        label="Number of cases"
-        labelSpace={68}
-        paddingEnd={0.1}
-        paddingStart={0}
-      />
-    </Plot>
-  );
-}*/
 export function Test() {
   return <PlotXY />;
 }
