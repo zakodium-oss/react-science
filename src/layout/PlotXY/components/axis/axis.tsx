615bbd8d69cb085fda761c0b356a13370933e5d9
fname lname <jw_bencheikh@esi.dz>
2021-12-20 21:16:57 +0100

    fix: delete useEffect in axis

--- src/layout/PlotXY/components/axis/axis.tsx
+++ src/layout/PlotXY/components/axis/axis.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { axisBottom, axisLeft, AxisScale } from 'd3-axis';
-import { select, selectAll } from 'd3-selection';
-import React, { useEffect } from 'react';
+import { select } from 'd3-selection';
+import React, { useMemo } from 'react';
 
 export default function Axis(props: {
   scale: AxisScale<number>;
@@ -10,31 +10,14 @@ export default function Axis(props: {
   transform: string;
 }) {
   const { scale, orient, ticks, transform } = props;
-  const ref = React.createRef<SVGGElement>();
-  useEffect(() => {
-    const renderAxis = () => {
-      const node: any = ref.current;
-      let axis: any;
+  const axis: any = useMemo(() => {
+    if (orient === 'bottom') {
+      return axisBottom(scale);
+    }
+    if (orient === 'left') {
+      return axisLeft(scale).ticks(ticks);
+    }
+  }, [orient, scale, ticks]);
 
-      if (orient === 'bottom') {
-        axis = axisBottom(scale);
-      }
-      if (orient === 'left') {
-        axis = axisLeft(scale).ticks(ticks);
-      }
-      select(node).call(axis);
-    };
-    const updateAxis = () => {
-      if (orient === 'left') {
-        const axis: any = axisLeft(scale).ticks(ticks);
-        selectAll(`.${orient}`).call(axis);
-      }
-    };
-    renderAxis();
-    return () => {
-      updateAxis();
-    };
-  }, [orient, ref, scale, ticks]);
-
-  return <g ref={ref} transform={transform} className={`${orient} axis`} />;
+  return <g ref={(node) => select(node).call(axis)} transform={transform} />;
 }
