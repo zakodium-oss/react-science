cf784315691c7ece15f6083f09cd38b50c9fce56
hamed-musallam <hamed.musallam@gmail.com>
2022-05-09 10:13:43 +0200

    refactor: remove unused component

--- src/layout/colorPicker/common/index.js
+++ src/layout/colorPicker/common/index.js
@@ -3,17 +3,7 @@ import CheckBoard from './CheckBoard';
 import ColorWrap from './ColorWrap';
 import EditableInput from './EditableInput';
 import Hue from './Hue';
-import Raised from './Raised';
 import Saturation from './Saturation';
 import Swatch from './Swatch';
 
-export {
-  Alpha,
-  CheckBoard,
-  EditableInput,
-  Hue,
-  Raised,
-  Saturation,
-  ColorWrap,
-  Swatch,
-};
+export { Alpha, CheckBoard, EditableInput, Hue, Saturation, ColorWrap, Swatch };
