f682091918a5ce09f12733d769894d75e260d161
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-05-27 16:20:57 +0100

    refactor: add props

--- src/components/MeasurementPlot.tsx
+++ src/components/MeasurementPlot.tsx
@@ -1,6 +1,17 @@
 import { xyToXYObject } from 'ml-spectra-processing';
-import React from 'react';
-import { Axis, Heading, LineSeries, Plot } from 'react-plot';
+import React, { useEffect, useMemo } from 'react';
+import {
+  Axis,
+  Heading,
+  LineSeries,
+  Plot,
+  PlotController,
+  useAxisWheelZoom,
+  useAxisZoom,
+  useCrossHair,
+  usePan,
+  useRectangularZoom,
+} from 'react-plot';
 
 interface Variable {
   label: string;
@@ -17,6 +28,14 @@ interface Measurement {
   info?: object;
   title?: string;
   data: Data[];
+  zoom?: 'horizontal' | 'vertical' | 'rectangular' | '';
+  wheelZoom?: 'vertical' | 'horizontal' | '';
+  crossHair?: boolean;
+  showHorizontalAxis?: boolean;
+  showVerticalAxis?: boolean;
+  showHorizontalGrid?: boolean;
+  showVerticalGrid?: boolean;
+  flipHorizontalAxis?: boolean;
 }
 export interface MeasurementPlotProps {
   measurement: Measurement;
@@ -27,6 +46,14 @@ export interface MeasurementPlotProps {
   height?: number;
 }
 export function MeasurementPlot(props: MeasurementPlotProps) {
+  return (
+    <PlotController>
+      <MeasurementComponent {...props} />
+    </PlotController>
+  );
+}
+
+function MeasurementComponent(props: MeasurementPlotProps) {
   const {
     measurement,
     dataIndex = 0,
@@ -35,10 +62,45 @@ export function MeasurementPlot(props: MeasurementPlotProps) {
     width = 800,
     height = 400,
   } = props;
-  const { title = '', data } = measurement;
+  const {
+    title = '',
+    data,
+    zoom = 'horizontal',
+    wheelZoom = 'vertical',
+    crossHair = true,
+    showHorizontalAxis = true,
+    showVerticalAxis = true,
+    showHorizontalGrid = true,
+    showVerticalGrid = true,
+    flipHorizontalAxis = false,
+  } = measurement;
   const {
     variables: { [xVariableName]: x, [yVariableName]: y },
   } = data[dataIndex];
+  useAxisZoom({
+    direction: zoom,
+    axisId: zoom === 'vertical' ? yVariableName : xVariableName,
+  });
+  useRectangularZoom({
+    horizontalAxisId: xVariableName,
+    verticalAxisId: yVariableName,
+  });
+  useAxisWheelZoom({
+    direction: wheelZoom,
+    axisId: wheelZoom === 'vertical' ? yVariableName : xVariableName,
+  });
+  useAxisWheelZoom({
+    direction: wheelZoom,
+    axisId: wheelZoom === 'vertical' ? yVariableName : xVariableName,
+  });
+  useCrossHair({
+    horizontalAxisId: xVariableName,
+    verticalAxisId: yVariableName,
+  });
+  usePan({
+    horizontalAxisId: xVariableName,
+    verticalAxisId: yVariableName,
+  });
   return (
     <Plot width={width} height={height}>
       <Heading title={title} />
@@ -49,12 +111,17 @@ export function MeasurementPlot(props: MeasurementPlotProps) {
         })}
       />
       <Axis
-        key="2"
+        id={xVariableName}
+        hidden={!showHorizontalAxis}
+        displayPrimaryGridLines={showHorizontalGrid}
+        flip={flipHorizontalAxis}
         position="bottom"
         label={`${x.label}${x.units ? `(${x.units})` : ''}`}
       />
       <Axis
-        key="3"
+        id={yVariableName}
+        hidden={!showVerticalAxis}
+        displayPrimaryGridLines={showVerticalGrid}
         position="left"
         label={`${y.label}${y.units ? `(${y.units})` : ''}`}
       />
