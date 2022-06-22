e45bb034f2d32ba6455360192e8b0a67b698fcc5
Michaël Zasso <targos@protonmail.com>
2022-06-22 15:19:50 +0200

    fix: specify base URL for gh-pages build (#156)
    Fixes: https://github.com/zakodium-oss/analysis-ui-components/issues/149

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    // TODO: https://github.com/Wildhoney/ReactShadow/pull/121
-    global: 'globalThis',
-  },
+  base: process.env.VITE_BASE || '/',
   build: {
     minify: process.env.NO_MINIFY ? false : 'esbuild',
   },
