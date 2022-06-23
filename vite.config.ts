f2259d39362c45ff2737ac5e55c8b4a68f952eb4
Luc Patiny <luc@patiny.com>
2022-06-23 16:18:20 +0200

    chore: config tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    // TODO: https://github.com/Wildhoney/ReactShadow/pull/121
-    global: 'globalThis',
-  },
   build: {
     minify: process.env.NO_MINIFY ? false : 'esbuild',
   },
