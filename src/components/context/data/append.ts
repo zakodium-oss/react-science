3b9c9de0a239904f6c1de4f606fcd9a5e82cbcaa
Luc Patiny <lpatiny@users.noreply.github.com>
2022-06-24 14:19:13 +0200

    feat: add DataState utilities
    * docs: add jsdoc on SplitPane options
    
    * chore: config tests
    
    * chore: add data examples
    
    * feat: create emptyDataState and append a filelist
    
    * feat: add enhancer and processor example
    
    * chore: fix package.json
    
    * chore: fix neta to meta
    
    * docs: add processor type
    
    * chore: fix eslint and specify Processor for jcampProcessor
    
    * chore: enable tests in CI
    
    * chore: remove library uuid
    
    * chore: add vite test config
    
    * chore: fix default append options
    
    Co-authored-by: Michaël Zasso <targos@protonmail.com>

---
+++ src/components/context/data/append.ts
@@ -0,0 +1,31 @@
+import { PartialFileList } from 'filelist-utils';
+import { produce } from 'immer';
+
+import { DataState, Processor } from './DataState';
+import { jcampProcessor } from './processors/jcampProcessor';
+
+const defaultProcessors = [jcampProcessor];
+
+interface AppendOptions {
+  processors?: Processor[];
+}
+/**
+ * Adds in the state the information that can be extracted from the fileList
+ * We can add 'processors' that based on the extension, filename, path or content
+ * could process the data before adding it in the state
+ */
+export async function append(
+  fileList: PartialFileList,
+  baseState: DataState,
+  options: AppendOptions = {},
+) {
+  const { processors = defaultProcessors } = options;
+
+  const nextDataState = await produce(baseState, async (draft) => {
+    for (const processor of processors) {
+      await processor(fileList, draft);
+    }
+  });
+
+  return { logs: [], dataState: nextDataState };
+}
