f7ee189c9142416c349051d149aef08239281e0a
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-06-29 23:44:48 +0100

    test: measurementExplorer test

--- tests/components/MeasurementExplorer.test.tsx
+++ tests/components/MeasurementExplorer.test.tsx
@@ -1,15 +1,59 @@
+/* eslint-disable jest/no-standalone-expect */
 import { test, expect } from '@playwright/experimental-ct-react';
 
-import { MeasurementPlot } from '../../src';
+import { MeasurementExplorer } from '../../src';
 import measurement from '../../stories/data/measurement.json';
 
-test.describe('MeasurementPlot test', () => {
-  test('should load passed hex color', async ({ mount }) => {
+test.describe('MeasurementExplorer test', () => {
+  test('test select', async ({ mount }) => {
     const component = await mount(
-      <MeasurementPlot measurement={measurement} />,
+      <MeasurementExplorer measurement={measurement} />,
     );
-    const dataIndex = component.locator('_react=select');
-    // console.log(dataIndex);
-    await expect(dataIndex).toHaveValue('0');
+    const dataIndex = component.locator('select >> nth=0');
+    const xVariableName = component.locator('select >> nth=1');
+    const yVariableName = component.locator('select >> nth=2');
+
+    await dataIndex.selectOption('1');
+    await xVariableName.selectOption('a');
+    await yVariableName.selectOption('t');
+    await expect(dataIndex).toHaveValue('1');
+    await expect(xVariableName).toHaveValue('a');
+    await expect(yVariableName).toHaveValue('t');
+  });
+  test.skip('test select same variable', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementExplorer measurement={measurement} />,
+    );
+    const xVariableName = component.locator('select >> nth=1');
+    const yVariableName = component.locator('select >> nth=2');
+
+    await xVariableName.selectOption('t');
+    await yVariableName.selectOption('t');
+    await expect(xVariableName).toHaveValue('t');
+    await expect(yVariableName).toHaveValue('t');
+  });
+  test('test reverse btn', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementExplorer measurement={measurement} />,
+    );
+
+    const reverseBtn = component.locator('_react=FaExchangeAlt');
+    const xVariableName = component.locator('select >> nth=1');
+    const yVariableName = component.locator('select >> nth=2');
+
+    await expect(xVariableName).toHaveValue('x');
+    await expect(yVariableName).toHaveValue('y');
+    await reverseBtn.click();
+    await expect(xVariableName).toHaveValue('y');
+    await expect(yVariableName).toHaveValue('x');
+  });
+  test('test flip btn', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementExplorer measurement={measurement} />,
+    );
+
+    // const MeasurementPlot = component.locator('_react=MeasurementPlot');
+    const flipBtn = component.locator('_react=FaArrowsAltH');
+    await flipBtn.click();
   });
 });
