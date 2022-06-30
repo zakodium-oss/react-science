ec5849b3c65859b181750708d309846c0626972c
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-06-30 20:07:33 +0100

    test: diffrent measurementPlot hooks tests

--- tests/components/MeasurementPlot.test.tsx
+++ tests/components/MeasurementPlot.test.tsx
@@ -4,6 +4,31 @@ import { test, expect } from '@playwright/experimental-ct-react';
 import { MeasurementPlot } from '../../src';
 import measurement from '../../stories/data/measurement.json';
 
+const beforeZoom = [
+  // horizontal axis default values
+  500,
+  1000,
+  1500,
+  2000,
+  2500,
+  3000,
+  3500,
+  '1/CM',
+
+  // vertical axis default values
+  40,
+  45,
+  50,
+  55,
+  60,
+  65,
+  70,
+  75,
+  80,
+  85,
+  '% Transmittance',
+].join('');
+
 test.describe('MeasurementPlot test', () => {
   test('test default', async ({ mount }) => {
     const component = await mount(
@@ -43,30 +68,7 @@ test.describe('MeasurementPlot test', () => {
     ).toHaveCount(10);
 
     // zoom (horizontal zoom)
-    const beforeZoom = [
-      // horizontal axis default values
-      500,
-      1000,
-      1500,
-      2000,
-      2500,
-      3000,
-      3500,
-      '1/CM',
 
-      // vertical axis default values
-      40,
-      45,
-      50,
-      55,
-      60,
-      65,
-      70,
-      75,
-      80,
-      85,
-      '% Transmittance',
-    ].join('');
     await expect(component).toContainText(beforeZoom);
     await component.dragTo(component, {
       sourcePosition: { x: 100, y: 100 },
@@ -128,5 +130,97 @@ test.describe('MeasurementPlot test', () => {
     );
     const xAxis = component.locator('_react=Axis[flip=true] >> nth=0');
     await expect(xAxis).toBeEnabled();
+  });
+  test('test vertical zoom', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementPlot measurement={measurement} zoom="vertical" />,
+    );
+    await expect(component).toContainText(beforeZoom);
+    await component.dragTo(component, {
+      sourcePosition: { x: 100, y: 100 },
+      targetPosition: { x: 200, y: 200 },
+    });
+    const afterZoom = [
+      // horizontal axis default values
+      500,
+      1000,
+      1500,
+      2000,
+      2500,
+      3000,
+      3500,
+      '1/CM',
+
+      // new vertical axis values after zoom
+      62,
+      64,
+      66,
+      68,
+      70,
+      72,
+      74,
+      '% Transmittance',
+    ].join('');
+    await expect(component).toContainText(afterZoom);
+  });
+  test('test rectangular zoom', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementPlot measurement={measurement} zoom="rectangular" />,
+    );
+    await expect(component).toContainText(beforeZoom);
+    await component.dragTo(component, {
+      sourcePosition: { x: 100, y: 100 },
+      targetPosition: { x: 200, y: 200 },
+    });
+    const afterZoom = [
+      // horizontal axis default values
+      700,
+      750,
+      800,
+      850,
+      900,
+      950,
+      1000,
+      1050,
+      1100,
+      '1/CM',
+
+      // new vertical axis values after zoom
+      62,
+      64,
+      66,
+      68,
+      70,
+      72,
+      74,
+      '% Transmittance',
+    ].join('');
+    await expect(component).toContainText(afterZoom);
+  });
+  test('test horizontal grid', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementPlot measurement={measurement} showHorizontalGrid={false} />,
+    );
+
+    await expect(
+      component.locator('_react=line[x1="0"][strokeDasharray= "2,2"]'),
+    ).toHaveCount(0);
+  });
+  test('test vertical grid', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementPlot measurement={measurement} showVerticalGrid={false} />,
+    );
+
+    await expect(
+      component.locator('_react=line[y2="0"][strokeDasharray= "2,2"]'),
+    ).toHaveCount(0);
+  });
+  test('test crossHair', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementPlot measurement={measurement} crossHair={false} />,
+    );
+    await component.hover({ position: { x: 200, y: 200 } });
+    await expect(component.locator('_react=Text')).toHaveCount(0);
+    await expect(component.locator('_react=Line')).toHaveCount(0);
   });
 });
