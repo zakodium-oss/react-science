4e25e5dbbdedcc84e3162f460546e1bdc719fa39
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-06-30 19:53:36 +0100

    refactor: test titles measurementExplorer

--- tests/components/MeasurementExplorer.test.tsx
+++ tests/components/MeasurementExplorer.test.tsx
@@ -5,7 +5,7 @@ import { MeasurementExplorer } from '../../src';
 import measurement from '../../stories/data/measurement.json';
 
 test.describe('MeasurementExplorer test', () => {
-  test('test init variables', async ({ mount }) => {
+  test('test initial variables', async ({ mount }) => {
     const component = await mount(
       <MeasurementExplorer measurement={measurement} />,
     );
@@ -17,7 +17,7 @@ test.describe('MeasurementExplorer test', () => {
     await expect(xVariableName).toHaveValue('x');
     await expect(yVariableName).toHaveValue('y');
   });
-  test('test select', async ({ mount }) => {
+  test('test select variables', async ({ mount }) => {
     const component = await mount(
       <MeasurementExplorer measurement={measurement} />,
     );
@@ -28,14 +28,19 @@ test.describe('MeasurementExplorer test', () => {
     await dataIndex.selectOption('1');
     await xVariableName.selectOption('a');
     await yVariableName.selectOption('t');
+
     await expect(dataIndex).toHaveValue('1');
     await expect(xVariableName).toHaveValue('a');
     await expect(yVariableName).toHaveValue('t');
 
-    await xVariableName.selectOption('y');
-    await yVariableName.selectOption('x');
-    await expect(xVariableName).toHaveValue('y');
-    await expect(yVariableName).toHaveValue('x');
+    // test MeasurementPlot props after select variables
+    await component.locator('_react=MeasurementPlot[dataIndex=1]').isEnabled();
+    await component
+      .locator('_react=MeasurementPlot[xVariableName="a"]')
+      .isEnabled();
+    await component
+      .locator('_react=MeasurementPlot[yVariableName="t"]')
+      .isEnabled();
   });
   test('test reverse btn', async ({ mount }) => {
     const component = await mount(
@@ -46,11 +51,23 @@ test.describe('MeasurementExplorer test', () => {
     const xVariableName = component.locator('select >> nth=1');
     const yVariableName = component.locator('select >> nth=2');
 
+    // test selected variables before reverse (default variables)
     await expect(xVariableName).toHaveValue('x');
     await expect(yVariableName).toHaveValue('y');
+
     await reverseBtn.click();
+
+    // test selected variables after reverse
     await expect(xVariableName).toHaveValue('y');
     await expect(yVariableName).toHaveValue('x');
+
+    // test MeasurementPlot props after reverse
+    await component
+      .locator('_react=MeasurementPlot[xVariableName="y"]')
+      .isEnabled();
+    await component
+      .locator('_react=MeasurementPlot[yVariableName="x"]')
+      .isEnabled();
   });
   test('test flip btn', async ({ mount }) => {
     const component = await mount(
@@ -65,11 +82,13 @@ test.describe('MeasurementExplorer test', () => {
       '_react=MeasurementPlot[flipHorizontalAxis=true]',
     );
 
+    // test MeasurementPlot props before flipping axis
     await noFlippedPlot.isEnabled();
     await FlippedPlot.waitFor({ state: 'detached' });
 
     await flipBtn.click();
 
+    // test MeasurementPlot props after flipping axis
     await noFlippedPlot.waitFor({ state: 'detached' });
     await FlippedPlot.isEnabled();
   });
