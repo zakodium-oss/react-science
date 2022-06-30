42f65a95a613c481223a621aab4b2385c445558a
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-06-30 13:10:44 +0100

    test: measurementExplorer

--- tests/components/MeasurementExplorer.test.tsx
+++ tests/components/MeasurementExplorer.test.tsx
@@ -5,6 +5,18 @@ import { MeasurementExplorer } from '../../src';
 import measurement from '../../stories/data/measurement.json';
 
 test.describe('MeasurementExplorer test', () => {
+  test('test init variables', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementExplorer measurement={measurement} />,
+    );
+    const dataIndex = component.locator('select >> nth=0');
+    const xVariableName = component.locator('select >> nth=1');
+    const yVariableName = component.locator('select >> nth=2');
+
+    await expect(dataIndex).toHaveValue('0');
+    await expect(xVariableName).toHaveValue('x');
+    await expect(yVariableName).toHaveValue('y');
+  });
   test('test select', async ({ mount }) => {
     const component = await mount(
       <MeasurementExplorer measurement={measurement} />,
@@ -19,18 +31,11 @@ test.describe('MeasurementExplorer test', () => {
     await expect(dataIndex).toHaveValue('1');
     await expect(xVariableName).toHaveValue('a');
     await expect(yVariableName).toHaveValue('t');
-  });
-  test.skip('test select same variable', async ({ mount }) => {
-    const component = await mount(
-      <MeasurementExplorer measurement={measurement} />,
-    );
-    const xVariableName = component.locator('select >> nth=1');
-    const yVariableName = component.locator('select >> nth=2');
 
-    await xVariableName.selectOption('t');
-    await yVariableName.selectOption('t');
-    await expect(xVariableName).toHaveValue('t');
-    await expect(yVariableName).toHaveValue('t');
+    await xVariableName.selectOption('y');
+    await yVariableName.selectOption('x');
+    await expect(xVariableName).toHaveValue('y');
+    await expect(yVariableName).toHaveValue('x');
   });
   test('test reverse btn', async ({ mount }) => {
     const component = await mount(
@@ -52,8 +57,20 @@ test.describe('MeasurementExplorer test', () => {
       <MeasurementExplorer measurement={measurement} />,
     );
 
-    // const MeasurementPlot = component.locator('_react=MeasurementPlot');
     const flipBtn = component.locator('_react=FaArrowsAltH');
+    const noFlippedPlot = component.locator(
+      '_react=MeasurementPlot[flipHorizontalAxis=false]',
+    );
+    const FlippedPlot = component.locator(
+      '_react=MeasurementPlot[flipHorizontalAxis=true]',
+    );
+
+    await noFlippedPlot.isEnabled();
+    await FlippedPlot.waitFor({ state: 'detached' });
+
     await flipBtn.click();
+
+    await noFlippedPlot.waitFor({ state: 'detached' });
+    await FlippedPlot.isEnabled();
   });
 });
