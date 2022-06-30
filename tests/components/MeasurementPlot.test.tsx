5585287f304867cab199807d2212f12fe1d1b2bc
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-06-30 19:53:48 +0100

    test: default values test measurementPlot

--- tests/components/MeasurementPlot.test.tsx
+++ tests/components/MeasurementPlot.test.tsx
@@ -12,9 +12,97 @@ test.describe('MeasurementPlot test', () => {
     const xAxis = component.locator('_react=Axis >> nth=0');
     const yAxis = component.locator('_react=Axis >> nth=1');
 
+    //default variables
     await expect(xAxis).toContainText('1/CM');
     await expect(yAxis).toContainText('% Transmittance');
     await expect(component).toContainText('Mattson Instruments');
+    await expect(component.locator('_react=Axis[id="x-x"]')).toBeEnabled();
+    await expect(component.locator('_react=Axis[id="y-y"]')).toBeEnabled();
+
+    // flip
+    await component
+      .locator('_react=Axis[flip=true] >> nth=0')
+      .waitFor({ state: 'detached' });
+
+    // crossHair
+    await component.hover({ position: { x: 200, y: 200 } });
+    await expect(component.locator('_react=Text')).toContainText(
+      '1148.22 ,60.33',
+    );
+    await expect(component.locator('_react=Line')).toHaveCount(2);
+    // grids
+
+    // vertical grid
+    await expect(
+      component.locator('_react=line[y2="0"][strokeDasharray= "2,2"]'),
+    ).toHaveCount(7);
+
+    // horizontal grid
+    await expect(
+      component.locator('_react=line[x1="0"][strokeDasharray= "2,2"]'),
+    ).toHaveCount(10);
+
+    // zoom (horizontal zoom)
+    const beforeZoom = [
+      // horizontal axis default values
+      500,
+      1000,
+      1500,
+      2000,
+      2500,
+      3000,
+      3500,
+      '1/CM',
+
+      // vertical axis default values
+      40,
+      45,
+      50,
+      55,
+      60,
+      65,
+      70,
+      75,
+      80,
+      85,
+      '% Transmittance',
+    ].join('');
+    await expect(component).toContainText(beforeZoom);
+    await component.dragTo(component, {
+      sourcePosition: { x: 100, y: 100 },
+      targetPosition: { x: 200, y: 200 },
+    });
+    const afterZoom = [
+      // new horizontal axis values after zoom
+      700,
+      750,
+      800,
+      850,
+      900,
+      950,
+      1000,
+      1050,
+      1100,
+      '1/CM',
+
+      // vertical axis default values
+      40,
+      45,
+      50,
+      55,
+      60,
+      65,
+      70,
+      75,
+      80,
+      85,
+      '% Transmittance',
+    ].join('');
+    await expect(component).toContainText(afterZoom);
+
+    // double Click
+    await component.dblclick();
+    await expect(component).toContainText(beforeZoom);
   });
   test('test change variables', async ({ mount }) => {
     const component = await mount(
@@ -22,13 +110,23 @@ test.describe('MeasurementPlot test', () => {
         measurement={measurement}
         xVariableName="y"
         yVariableName="a"
-        dataIndex={0}
+        dataIndex={1}
       />,
     );
     const xAxis = component.locator('_react=Axis >> nth=0');
     const yAxis = component.locator('_react=Axis >> nth=1');
 
-    await expect(xAxis).toContainText('% Transmittance');
+    await expect(component.locator('_react=Axis[id="y-x"]')).toBeEnabled();
+    await expect(component.locator('_react=Axis[id="a-y"]')).toBeEnabled();
+
+    await expect(xAxis).toContainText('TRANSMITTANCE');
     await expect(yAxis).toContainText('Absorbance');
+  });
+  test('test flip axis', async ({ mount }) => {
+    const component = await mount(
+      <MeasurementPlot measurement={measurement} flipHorizontalAxis />,
+    );
+    const xAxis = component.locator('_react=Axis[flip=true] >> nth=0');
+    await expect(xAxis).toBeEnabled();
   });
 });
