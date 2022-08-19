3593f311ee7733d5fa7af35bc3f53213a8490ab6
Michaël Zasso <targos@protonmail.com>
2022-08-19 09:00:21 +0200

    fix

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -1,7 +1,7 @@
+// Use optional chaining as the config ends up in the browser build.
+const viteBase = globalThis.process?.env?.VITE_BASE;
+
 export default {
   stories: 'stories/**/*.stories.{js,jsx,ts,tsx}',
-  // Use optional chaining as the config ends up in the browser build.
-  base: process?.env?.VITE_BASE
-    ? process?.env?.VITE_BASE + 'stories/'
-    : '/stories/',
+  base: viteBase ? viteBase + 'stories/' : '/stories/',
 };
