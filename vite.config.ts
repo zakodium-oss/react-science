f9d1df64a7760724e0b2ae683433cb6dbc088214
Daniel Kostro <kostro.d@gmail.com>
2022-09-23 11:49:38 +0200

    chore: fix issues with ladle and playwright build configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vitest/config';
-import { splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
+import { splitVendorChunkPlugin } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+const plugins = [react(), splitVendorChunkPlugin()];
+
+if (!process.env.VITE_PUBLIC_LADLE_THEME) {
+  plugins.push(tsconfigPaths());
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +18,7 @@ export default defineConfig({
   build: {
     minify: process.env.NO_MINIFY ? false : 'esbuild',
   },
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins,
   test: {
     globals: true,
     dir: 'src',
