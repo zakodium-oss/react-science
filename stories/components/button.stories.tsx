2dd45e0922ccf91fbde4d0e54b2c8e59f47a0acf
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-10-10 11:45:32 +0100

    test: add toggle fullscreen in stories

--- stories/components/button.stories.tsx
+++ stories/components/button.stories.tsx
@@ -1,4 +1,8 @@
 import { Button } from '@/components';
+import {
+  FullScreenProvider,
+  useFullscreen,
+} from '@/components/context/FullscreenContext';
 
 export default {
   title: 'Components / Button',
@@ -6,4 +10,19 @@ export default {
 
 export function Basic() {
   return <Button>Hello, World!</Button>;
+}
+export function Fullscreen() {
+  return (
+    <FullScreenProvider>
+      <Content />
+    </FullScreenProvider>
+  );
+}
+function Content() {
+  const { toggle } = useFullscreen();
+  return (
+    <div>
+      <Button onClick={toggle}>Click to toggle fullscreen</Button>
+    </div>
+  );
 }
