4bf2a26be2fdd3eab1c28b6dea32b467ccdf8db7
Luc Patiny <lpatiny@users.noreply.github.com>
2022-10-15 20:45:19 +0200

    cary500 (#247)
    * wip: prepare cary500Loader
    
    * update!: support for fileCollection and guess the spectra type (#246)
    
    * update: support for fileCollection and classification of spectra
    
    * update: a few missing types, and prettify
    
    * test: disable temporarely getIVMeasurement
    
    * chore: rename files to fileCollection
    
    * chore: add todo to use FiltCollection.filter
    
    Co-authored-by: Luc Patiny <luc@patiny.com>
    
    * feat: add experimental cary500Loader
    
    * chore: update dependencies and add testcase
    
    * chore: add missing vitest import
    
    * chore: fix prettier
    
    Co-authored-by: Santiago Miranda <santimir@protonmail.com>

---
+++ src/app/data/loaders/proprietary/agilent/__tests__/cary500Loader.test.ts
@@ -0,0 +1,14 @@
+import { join } from 'path';
+
+import { fileCollectionFromPath } from 'filelist-utils';
+import { expect, test } from 'vitest';
+
+import { cary500Loader } from '../cary500Loader';
+
+test('cary500Loader', async () => {
+  const fileCollection = await fileCollectionFromPath(join(__dirname, 'data'));
+  // we should filter to keep only the right data
+  const results = await cary500Loader(fileCollection);
+
+  expect(results.uvvis.entries).toHaveLength(8);
+});
