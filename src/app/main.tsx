43adbe8737760eab6db24acca0ed53a588716a4d
Luc Patiny <lpatiny@users.noreply.github.com>
2022-10-27 16:43:10 +0200

    chore: add direct links to test files in app-dev (#289)

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -8,20 +8,49 @@ import './main.css';
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
     <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-      {import.meta.env.PROD ? (
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            borderBottom: '1px solid black',
-            lineHeight: 1,
-            paddingBlock: 5,
-          }}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          borderBottom: '1px solid black',
+          gap: '10px',
+          lineHeight: 1,
+          paddingBlock: 5,
+        }}
+      >
+        <a
+          href={`${
+            import.meta.env.BASE_URL
+          }?filelist=https://zakodium-oss.github.io/analysis-dataset/jdx.json`}
         >
+          JCAMP-DX
+        </a>
+        <a
+          href={`${
+            import.meta.env.BASE_URL
+          }?filelist=https://zakodium-oss.github.io/analysis-dataset/biologic.json`}
+        >
+          Biologic
+        </a>
+        <a
+          href={`${
+            import.meta.env.BASE_URL
+          }?filelist=https://zakodium-oss.github.io/analysis-dataset/uvvis.json`}
+        >
+          UV-vis
+        </a>
+        <a
+          href={`${
+            import.meta.env.BASE_URL
+          }?filelist=https://zakodium-oss.github.io/analysis-dataset/full.json`}
+        >
+          All the data we have
+        </a>
+        {import.meta.env.PROD ? (
           <a href={`${import.meta.env.BASE_URL}stories/`}>Open stories</a>
-        </div>
-      ) : null}
+        ) : null}
+      </div>
       <App />
     </div>
   </StrictMode>,
