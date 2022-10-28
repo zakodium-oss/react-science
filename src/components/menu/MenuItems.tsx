c8e4e1b4cdbbb27e3ffaa40b5189653ee8756c08
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2022-10-28 21:53:41 +0200

    feat: add icon gestion on Dropdown

--- src/components/menu/MenuItems.tsx
+++ src/components/menu/MenuItems.tsx
@@ -1,4 +1,4 @@
-import { ClassNames, ClassNamesContent } from '@emotion/react';
+import styled from '@emotion/styled';
 import { Menu } from '@headlessui/react';
 import type { ReactNode } from 'react';
 
@@ -24,6 +24,7 @@ export interface MenuItemsProps<T> {
 
 interface ItemProps<T> {
   option: MenuOptions<T>[number];
+  dividerSpan: number;
   onSelect: MenuItemsProps<T>['onSelect'];
 }
 
@@ -31,114 +32,128 @@ interface ItemOptionProps<T> {
   option: MenuOption<T>;
   onSelect: MenuItemsProps<T>['onSelect'];
   active: boolean;
+  hasOneIconOrMore: boolean;
 }
 
-const styles = (css: ClassNamesContent['css']) => {
-  return {
-    items: css`
-      display: grid;
-      grid-template-columns: 40px auto;
-      width: fit-content;
-      align-items: center;
-      border-radius: 6px;
-      background-color: white;
-      box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
-        rgba(0, 0, 0, 0.22) 0px 5px 12px;
-      padding-top: 5px;
-      padding-bottom: 5px;
-    `,
-    item: (active: boolean, option: MenuOption<any>) =>
-      css`
-        display: contents;
-        cursor: ${option.disabled ? 'default' : 'pointer'};
-        font-size: 0.875rem;
-        color: ${!option.disabled ? 'black' : 'rgb(163, 163, 163)'};
-        & > div {
-          padding-top: 2px;
-          padding-bottom: 2px;
-          ${active && 'background-color: rgb(243, 244, 246);'}
-        }
+const ItemDiv = styled.div<{
+  disabled: boolean;
+  active: boolean;
+}>`
+  display: contents;
+  cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
+  font-size: 0.875rem;
+  color: ${(props) => (!props.disabled ? 'black' : 'rgb(163, 163, 163)')};
+  & > div {
+    padding-top: 2px;
+    padding-bottom: 2px;
+    ${(props) => props.active && 'background-color: rgb(243, 244, 246);'}
+  }
 
-        ${!option.disabled &&
-        `
+  ${(props) =>
+    !props.disabled &&
+    `
           &:hover > div {
             background-color: rgb(243, 244, 246);
           }
         `}
-      `,
-    divider: css`
-      width: 100%;
-      color: rgb(229, 229, 229);
-      margin-top: 5px;
-      margin-bottom: 5px;
-      grid-column: 1 / span 2;
-    `,
-  };
-};
+`;
+
+const Divider = styled.hr<{ span: number }>`
+  width: 100%;
+  color: rgb(229, 229, 229);
+  margin-top: 5px;
+  margin-bottom: 5px;
+  grid-column: 1 / span ${(props) => props.span};
+`;
+
+const ItemsDiv = styled.div<{
+  hasOneIconOrMore: boolean;
+}>`
+  display: grid;
+  grid-template-columns: ${(props) => props.hasOneIconOrMore && '40px'} auto;
+  width: fit-content;
+  align-items: center;
+  border-radius: 6px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 5px 12px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+
+const LabelDiv = styled.div<{ hasOneIconOrMore: boolean }>`
+  padding-right: 1rem;
+  ${(props) => !props.hasOneIconOrMore && `padding-left: 1rem;`}
+`;
 
 export function MenuItems<T>(props: MenuItemsProps<T>) {
   const { options, onSelect, itemsStatic } = props;
+  const hasOneOrMoreIcon = options.some(
+    (option) => option.type === 'option' && option.icon,
+  );
 
   return (
-    <ClassNames>
-      {({ css }) => (
-        <Menu.Items static={itemsStatic} className={styles(css).items}>
-          {options.map((option, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <Item key={index} onSelect={onSelect} option={option} />
-          ))}
-        </Menu.Items>
-      )}
-    </ClassNames>
+    <Menu.Items
+      as={ItemsDiv}
+      static={itemsStatic}
+      hasOneIconOrMore={hasOneOrMoreIcon}
+    >
+      {options.map((option, index) => (
+        <Item
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          onSelect={onSelect}
+          option={option}
+          dividerSpan={hasOneOrMoreIcon ? 2 : 1}
+        />
+      ))}
+    </Menu.Items>
   );
 }
 
 function Item<T>(props: ItemProps<T>) {
-  const { option, onSelect } = props;
+  const { option, onSelect, dividerSpan } = props;
   const isDivider = option.type === 'divider';
 
   if (isDivider) {
-    return (
-      <ClassNames>
-        {({ css }) => <hr className={styles(css).divider} />}
-      </ClassNames>
-    );
+    return <Divider span={dividerSpan} />;
   }
 
   return (
     <Menu.Item disabled={option.disabled}>
       {({ active }) => (
-        <ItemOption onSelect={onSelect} option={option} active={active} />
+        <ItemOption
+          onSelect={onSelect}
+          option={option}
+          active={active}
+          hasOneIconOrMore={dividerSpan === 2}
+        />
       )}
     </Menu.Item>
   );
 }
 
 function ItemOption<T>(props: ItemOptionProps<T>) {
-  const { onSelect, option, active } = props;
+  const { onSelect, option, active, hasOneIconOrMore } = props;
 
   return (
-    <ClassNames>
-      {({ css }) => (
-        <div
-          onClick={() => onSelect(option)}
-          className={styles(css).item(active, option)}
-        >
-          <div
-            style={{
-              width: '100%',
-              height: '100%',
-              paddingLeft: '1rem',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            {option.icon}
-          </div>
-          <div style={{ paddingRight: '1rem' }}>{option.label}</div>
-        </div>
-      )}
-    </ClassNames>
+    <ItemDiv
+      onClick={() => onSelect(option)}
+      active={active}
+      disabled={option.disabled || false}
+    >
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          paddingLeft: '1rem',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {option.icon}
+      </div>
+      <LabelDiv hasOneIconOrMore={hasOneIconOrMore}>{option.label}</LabelDiv>
+    </ItemDiv>
   );
 }
