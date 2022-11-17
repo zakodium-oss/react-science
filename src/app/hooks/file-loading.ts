f3581161e383ff0bbe2ebef843ea111cf7e55ec7
Michaël Zasso <targos@protonmail.com>
2022-11-17 10:58:44 +0100

    chore: return something from query callback

--- src/app/hooks/file-loading.ts
+++ src/app/hooks/file-loading.ts
@@ -22,6 +22,7 @@ export function useLoadFileCollectionFromHash(onLoad: LoadFn) {
       }
       const fileCollection = await fileCollectionFromWebservice(filelistUrl);
       void onLoad(fileCollection, appDispatch);
+      return true;
     },
   });
   return query;
