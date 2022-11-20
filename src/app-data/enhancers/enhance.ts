3c2c44cf4d4167e30dd86dc5e49b518830b5d8d1
Santi Miranda <santimir@protonmail.com>
2022-11-20 20:53:18 +0100

    refactor(enhancers): add type, remove anys

--- src/app-data/enhancers/enhance.ts
+++ src/app-data/enhancers/enhance.ts
@@ -1,14 +1,23 @@
 import type { Measurements } from '../index';
+import { MeasurementBase } from '../state/index';
 
-export function enhance(measurements: Partial<Measurements>, enhancers = {}) {
-  for (let key in measurements) {
-    if (
-      enhancers[key] &&
-      enhancers[key].length > 0 &&
-      measurements[key].entries.length > 0
-    ) {
-      for (const measurement of measurements[key].entries) {
-        for (const enhancer of enhancers[key]) {
+export type Enhancer<T extends MeasurementBase> = (data: T) => void;
+
+export type Enhancers = {
+  [mKind in keyof Partial<Measurements>]: Enhancer<
+    Measurements[mKind]['entries'][number]
+  >[];
+};
+
+export function enhance(
+  measurements: Partial<Measurements>,
+  enhancers: Partial<Enhancers>,
+) {
+  for (const [kind, kindEnhancers] of Object.entries(enhancers)) {
+    const kindMeasurements = measurements[kind as keyof Measurements];
+    if (kindMeasurements) {
+      for (const measurement of kindMeasurements.entries) {
+        for (const enhancer of kindEnhancers) {
           enhancer(measurement);
         }
       }
