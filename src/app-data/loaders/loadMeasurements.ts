919b50c568f7a0f2b757452010f36f73936bd399
Michaël Zasso <targos@protonmail.com>
2022-11-29 14:54:13 +0100

    fix: compute missing variables min/max while loading measurements

--- src/app-data/loaders/loadMeasurements.ts
+++ src/app-data/loaders/loadMeasurements.ts
@@ -1,6 +1,7 @@
 import type { FileCollection } from 'filelist-utils';
+import { xMinMaxValues } from 'ml-spectra-processing';
 
-import { Measurements, enhance, Enhancers } from '../index';
+import { Measurements, enhance, Enhancers, MeasurementBase } from '../index';
 
 import {
   MeasurementsLoader,
@@ -23,8 +24,32 @@ export async function loadMeasurements(
   const { loaders = [], enhancers = {}, logger = true } = options;
   for (const loader of loaders) {
     const loaderData = await loader(fileCollection, logger ? logs : undefined);
+    for (const { entries } of Object.values(loaderData)) {
+      for (const entry of entries) {
+        computeMinMax(entry);
+      }
+    }
     enhance(loaderData, enhancers);
     mergeMeasurements(measurements, loaderData);
   }
   return { measurements, logs };
+}
+
+function computeMinMax(measurement: MeasurementBase) {
+  for (const datum of measurement.data) {
+    for (const variable of Object.values(datum.variables)) {
+      if (variable.data.length === 0) {
+        // TODO: should we accept empty data?
+        return;
+      }
+      if (
+        typeof variable.min === 'undefined' ||
+        typeof variable.max === 'undefined'
+      ) {
+        const { min, max } = xMinMaxValues(variable.data);
+        variable.min = min;
+        variable.max = max;
+      }
+    }
+  }
 }
