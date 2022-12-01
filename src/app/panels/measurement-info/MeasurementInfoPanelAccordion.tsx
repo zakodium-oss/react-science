024f9ea53d8ccf9c1afd973fb16f59897c2c6dc9
Michaël Zasso <targos@protonmail.com>
2022-12-01 15:34:06 +0100

    fix: never hide measurement info panel

--- src/app/panels/measurement-info/MeasurementInfoPanelAccordion.tsx
+++ src/app/panels/measurement-info/MeasurementInfoPanelAccordion.tsx
@@ -1,17 +1,10 @@
-import {
-  getCurrentMeasurementData,
-  useAppState,
-} from '../../../app-data/index';
 import { Accordion } from '../../../components/index';
 
 import { MeasurementInfoPanel } from './MeasurementInfoPanel';
 
 export function MeasurementInfoPanelAccordion() {
-  const appState = useAppState();
-  const measurement = getCurrentMeasurementData(appState);
-  if (!measurement) return null;
   return (
-    <Accordion.Item title="Measurement info" defaultOpened>
+    <Accordion.Item title="Measurement info">
       <MeasurementInfoPanel />
     </Accordion.Item>
   );
