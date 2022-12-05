70a81c8fd298fee555d19e770c5d89f615a1daf2
Michaël Zasso <targos@protonmail.com>
2022-12-05 10:00:42 +0100

    fix: make build work for publication (#435)

--- scripts/fix-react-shadow.mjs
+++ scripts/fix-react-shadow.mjs
@@ -1,14 +1,14 @@
 import { readFile, writeFile } from 'node:fs/promises';
 
 const rootLayoutFile = new URL(
-  '../lib/components/RootLayout.js',
+  '../lib/components/root-layout/RootLayout.js',
   import.meta.url,
 );
 
 const rootLayout = await readFile(rootLayoutFile, 'utf8');
 if (!rootLayout.includes('react-shadow/emotion')) {
   throw new Error(
-    'react-shadow/emotion not found in lib/components/RootLayout.js',
+    'react-shadow/emotion not found in lib/components/root-layout/RootLayout.js',
   );
 }
 
