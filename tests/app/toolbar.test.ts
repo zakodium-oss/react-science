869f5ed796e23f6b200c2380c3c32669563a4d19
Michaël Zasso <targos@protonmail.com>
2022-12-08 15:15:12 +0100

    test: adapt test to layout changes

--- tests/app/toolbar.test.ts
+++ tests/app/toolbar.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('toolbar', async ({ page }) => {
   await page.goto('http://localhost:5173/pages/demo.html');
-  await expect(page.locator('_react=ToolbarItem')).toHaveCount(7);
+  await expect(page.locator('_react=ToolbarItem')).toHaveCount(5);
 });
