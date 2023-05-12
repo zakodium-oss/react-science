5f192fba8c7c477317b237ea392d8b2d024fe53e
moonayyur <jm_kias@esi.dz>
2023-05-12 20:53:05 +0100

    fix: fix prettier and build folder

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,15 +2,19 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   core: {},
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {
-      strictMode: false
-    }
+      strictMode: false,
+    },
   },
   docs: {
-    autodocs: 'tag'
-  }
+    autodocs: 'tag',
+  },
 };
 export default config;
