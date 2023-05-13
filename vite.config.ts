8100b39691c8473180df5d82846f90ba7f76ec78
moonayyur <jm_kias@esi.dz>
2023-05-13 15:07:44 +0100

    fix: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
 import react from '@vitejs/plugin-react';
 import { splitVendorChunkPlugin } from 'vite';
 import { defineConfig } from 'vitest/config';
 
 const plugins = [react(), splitVendorChunkPlugin()];
+
+const pages = fs.readdirSync(path.join(__dirname, 'pages'));
+
+const rollupOptions = {
+  input: {
+    index: path.join(__dirname, 'index.html'),
+    ...Object.fromEntries(
+      pages.map((page) => [page, path.join(__dirname, 'pages', page)]),
+    ),
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +31,7 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    rollupOptions,
     minify: process.env.NO_MINIFY ? false : 'esbuild',
   },
   plugins,
