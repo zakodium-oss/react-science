a9f89a0575862aa6632a773b4751013dbc8bf1f2
Xavier Stouder <xavier@stouder.io>
2023-05-23 13:11:27 +0200

    fix(tabs): ensure consistant behavior between horizontal and vertical content sizing

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -152,7 +152,7 @@ function TabsHorizontal<T extends string = string>(
   const item = useTabsContext();
   return (
     <div
-      style={{ display: 'flex', flexDirection: 'column', overflowX: 'hidden' }}
+      style={{ display: 'flex', flexDirection: 'column', overflowX: 'hidden', flex: '1 1 0%', height: '100%' }}
     >
       <div
         style={{
