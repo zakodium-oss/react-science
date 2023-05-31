48d4cd3504821d7d8d17d759b6dfb91596b8da7a
Xavier Stouder <xavier@stouder.io>
2023-05-31 12:54:34 +0200

    fix: add story for controlled, indeterminate and disabled checkbox

--- stories/components/checkbox.stories.tsx
+++ stories/components/checkbox.stories.tsx
@@ -67,3 +67,50 @@ export function ControlledAndIndeterminate() {
     </div>
   );
 }
+
+export function ControlledIndeterminateAndDisabled() {
+  const [orange, setOrange] = useState<boolean | 'indeterminate'>(false);
+  const [apple, setApple] = useState<boolean | 'indeterminate'>(false);
+  const [banana, setBanana] = useState<boolean | 'indeterminate'>(false);
+
+  const allChecked = useMemo(() => {
+    if (orange && apple && banana) return true;
+    if (!orange && !apple && !banana) return false;
+    return 'indeterminate';
+  }, [apple, banana, orange]);
+
+  const setAllChecked = useCallback((checked: boolean | 'indeterminate') => {
+    setOrange(checked);
+    setApple(checked);
+    setBanana(checked);
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        height: '100%',
+      }}
+    >
+      <div>
+        <Checkbox
+          checked={allChecked}
+          label="All checked"
+          onChange={setAllChecked}
+        />
+      </div>
+      <div style={{ display: 'flex' }}>
+        <Checkbox
+          disabled
+          checked={orange}
+          onChange={setOrange}
+          label="Orange"
+        />
+        <Checkbox checked={apple} onChange={setApple} label="Apple" />
+        <Checkbox checked={banana} onChange={setBanana} label="Banana" />
+      </div>
+    </div>
+  );
+}
