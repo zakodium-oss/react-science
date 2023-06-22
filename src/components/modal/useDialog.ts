e0254ee19174149ce8ae0dafc7ebe185827429aa
Xavier Stouder <xavier@stouder.io>
2023-06-22 15:28:02 +0200

    fix: click misdetected on chrome

--- src/components/modal/useDialog.ts
+++ src/components/modal/useDialog.ts
@@ -42,14 +42,27 @@ export function useDialog({
 
   const onClick = useCallback<MouseEventHandler<HTMLDialogElement>>(
     (event) => {
+      const dialog = dialogRef.current;
+      if (!dialog) {
+        return;
+      }
+
+      // Ref: https://stackoverflow.com/questions/25864259/how-to-close-the-new-html-dialog-tag-by-clicking-on-its-backdrop
+      const rect = dialog.getBoundingClientRect();
+      const isInDialog =
+        rect.top <= event.clientY &&
+        event.clientY <= rect.top + rect.height &&
+        rect.left <= event.clientX &&
+        event.clientX <= rect.left + rect.width;
+
       event.stopPropagation();
       // Since the dialog has no size of itself, this condition is only
       // `true` when we click on the backdrop.
-      if (event.target === event.currentTarget && requestCloseOnBackdrop) {
+      if (!isInDialog && requestCloseOnBackdrop) {
         onRequestClose?.();
       }
     },
-    [requestCloseOnBackdrop, onRequestClose],
+    [dialogRef, requestCloseOnBackdrop, onRequestClose],
   );
 
   return { onClick, onCancel };
