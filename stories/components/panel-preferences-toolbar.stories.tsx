350b15c9dde28fbd8bf5a46b218392d00efb049e
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2023-07-30 13:10:41 +0100

    test: panel-preferences-toolbar storybook

---
+++ stories/components/panel-preferences-toolbar.stories.tsx
@@ -0,0 +1,52 @@
+import {
+  Accordion,
+  Button,
+  PanelPreferencesToolbar,
+  useOnOff,
+} from '../../src/components';
+
+export default {
+  title: 'Components / PanelPreferencesToolbar',
+  component: PanelPreferencesToolbar,
+};
+
+export function Basic() {
+  return (
+    <div style={{ width: 300, height: 300 }}>
+      <PanelPreferencesToolbar>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, adipisci
+          culpa. Sapiente nesciunt perferendis repellendus. Voluptatum, cumque,
+          autem maiores eaque non vero, ut tempora esse possimus corporis ex
+          doloribus culpa!
+        </p>
+      </PanelPreferencesToolbar>
+    </div>
+  );
+}
+
+export function WithAccordion() {
+  const [isOpen, open, close] = useOnOff();
+
+  return (
+    <div
+      style={{
+        height: 300,
+      }}
+    >
+      <Accordion>
+        <Accordion.Item title="First Item" defaultOpened>
+          {isOpen ? (
+            <PanelPreferencesToolbar onSave={close} onClose={close}>
+              This is preferences content
+            </PanelPreferencesToolbar>
+          ) : (
+            <div>
+              <Button onClick={open}>Click to open preferences</Button>
+            </div>
+          )}
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+}
