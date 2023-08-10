aa7d573156a17f65b02ae8ffb83e942487ae7fe0
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2023-08-10 12:37:21 +0100

    chore: add storysource storybook addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   core: {},
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource'],
   framework: {
     name: '@storybook/react-vite',
     options: {
