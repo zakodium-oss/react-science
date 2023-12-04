bab808e220ab88bcdf52f0400b1f940c9dc5d6f2
hamed musallam <hamed.musallam@gmail.com>
2023-12-04 09:49:57 +0100

    feat: render custom value for boolean values

--- src/components/value-renderers/Boolean.tsx
+++ src/components/value-renderers/Boolean.tsx
@@ -2,10 +2,15 @@ import type { ValueRenderersProps } from '.';
 
 interface BooleanProps extends ValueRenderersProps {
   value?: boolean;
+  renderValue?: (value: boolean) => string;
 }
 
-export function Boolean({ value, ...other }: BooleanProps) {
-  return (
-    <div {...other}>{value !== undefined ? (value ? '✔' : '✘') : ''}</div>
-  );
+export function Boolean(props: BooleanProps) {
+  const {
+    value,
+    renderValue = (value) => (value ? '✔' : '✘'),
+    ...other
+  } = props;
+
+  return <div {...other}>{value !== undefined ? renderValue(value) : ''}</div>;
 }
