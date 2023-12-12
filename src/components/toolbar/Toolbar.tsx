d747a615ce251e46b1d2fa1525fe2bd19fb85516
hamed musallam <hamed.musallam@gmail.com>
2023-12-12 15:00:04 +0100

    fix: toolbar item width

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -115,7 +115,7 @@ Toolbar.Item = function ToolbarItem(props: ToolbarItemProps) {
         }
       `}
       intent={intent}
-      style={{ position: 'relative', fontSize: '1.25em', width: 'auto' }}
+      style={{ position: 'relative', fontSize: '1.25em', width: 'fit-content' }}
       type="button"
       active={active}
       icon={icon}
