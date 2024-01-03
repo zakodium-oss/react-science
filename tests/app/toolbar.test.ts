6e90154a037c6a20845fcb3c76a0f3b4987cd9dd
Daniel Kostro <kostro.d@gmail.com>
2024-01-03 17:24:28 +0100

    Revert "chore: introduce errors to test report workflow"
    This reverts commit 739515ace8ed9ab2568f4d39150ee764ef6a3f00.

--- tests/app/toolbar.test.ts
+++ tests/app/toolbar.test.ts
@@ -1,7 +1,6 @@
-import { expect, test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('toolbar', async ({ page }) => {
   await page.goto('http://localhost:5173/pages/demo.html');
   await expect(page.locator('_react=ToolbarItem')).toHaveCount(5);
-  throw new Error('test');
 });
--- tests/components/color-picker.test.tsx
+++ tests/components/color-picker.test.tsx
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/experimental-ct-react';
+import { test, expect } from '@playwright/experimental-ct-react';
 
 import { ColorPicker } from '../../src/components';
 
@@ -6,5 +6,4 @@ test('should load passed hex color', async ({ mount }) => {
   const component = await mount(<ColorPicker color={{ hex: '0099ff' }} />);
   const input = component.locator('_react=EditableInput >> nth=0 >> input');
   await expect(input).toHaveValue('0099FF');
-  throw new Error('test');
 });
