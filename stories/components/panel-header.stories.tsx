697de9277487877e6be74d41e9f5436cd662b5a9
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2024-01-19 21:44:09 +0100

    refactor: add accordion + panel toolbar + stacked panel story

--- stories/components/panel-header.stories.tsx
+++ stories/components/panel-header.stories.tsx
@@ -1,3 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import { Panel, PanelStack2 } from '@blueprintjs/core';
+import { PanelProps } from '@blueprintjs/core/lib/esnext/index';
+import { css } from '@emotion/react';
+import { ReactNode, useCallback, useState } from 'react';
+
 import {
   PanelHeader,
   Accordion,
@@ -5,6 +11,7 @@ import {
   Modal,
   useOnOff,
   Toolbar,
+  PanelPreferencesToolbar,
 } from '../../src/components';
 
 export default {
@@ -92,6 +99,135 @@ export function WithAccordion() {
             </Toolbar>
           </PanelHeader>
           This is the first content
+        </Accordion.Item>
+        <Accordion.Item title="Second Item">
+          <PanelHeader current={2} total={3} onClickSettings={() => void 0}>
+            <Button minimal icon="filter" />
+          </PanelHeader>
+          This is the content of the second item
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+}
+interface PanelInfo {
+  content: ReactNode;
+}
+function PanelContainer({ content }: PanelProps<PanelInfo>) {
+  return (
+    <div style={{ width: '100%', backgroundColor: 'grey' }}>{content}</div>
+  );
+}
+
+export function WithAccordionPanelToolbarStackedPanel() {
+  const [currentPanelStack, setCurrentPanelStack] = useState<
+    Array<Panel<PanelInfo>>
+  >([]);
+  const addToPanelStack = useCallback(
+    (newPanel: Panel<PanelInfo>) =>
+      setCurrentPanelStack((stack) => [...stack, newPanel]),
+    [],
+  );
+  const removeFromPanelStack = useCallback(
+    () => setCurrentPanelStack((stack) => stack.slice(0, -1)),
+    [],
+  );
+
+  return (
+    <div
+      style={{
+        height: 300,
+      }}
+    >
+      <Accordion>
+        <Accordion.Item title="First Item" defaultOpened>
+          {currentPanelStack.length === 0 ? (
+            <PanelHeader total={20} onClickSettings={() => void 0}>
+              <Toolbar>
+                <Toolbar.Item
+                  noTooltip
+                  title="trash"
+                  intent="danger"
+                  icon="trash"
+                  onClick={() =>
+                    addToPanelStack({
+                      props: {
+                        content: 'Panel1',
+                      },
+                      renderPanel: PanelContainer,
+                      title: 'Panel 1',
+                    })
+                  }
+                />
+                <Toolbar.Item
+                  onClick={() =>
+                    addToPanelStack({
+                      props: {
+                        content: `Panel2 ................................................
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+                        dignissimos fugit porro debitis cupiditate similique quam exercitationem
+                        natus hic delectus iste inventore itaque, dolorum eveniet ipsa sapiente
+                        voluptatibus a ipsam. Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Repellendus dignissimos fugit porro debitis cupiditate similique
+                        quam exercitationem natus hic delectus iste inventore itaque, dolorum
+                        eveniet ipsa sapiente voluptatibus a ipsam. Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Repellendus dignissimos fugit porro debitis
+                        cupiditate similique quam exercitationem natus hic delectus iste inventore
+                        itaque, dolorum eveniet ipsa sapiente voluptatibus a ipsam. Lorem ipsum
+                        dolor sit amet consectetur adipisicing elit. Repellendus dignissimos fugit
+                        porro debitis cupiditate similique quam exercitationem natus hic delectus
+                        iste inventore itaque, dolorum eveniet ipsa sapiente voluptatibus a ipsam.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+                        dignissimos fugit porro debitis cupiditate similique quam exercitationem
+                        natus hic delectus iste inventore itaque, dolorum eveniet ipsa sapiente
+                        voluptatibus a ipsam. Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Repellendus dignissimos fugit porro debitis cupiditate similique',`,
+                      },
+                      renderPanel: PanelContainer,
+                      title: 'Panel 2',
+                    })
+                  }
+                  noTooltip
+                  title="filter"
+                  icon="filter"
+                />
+                <Toolbar.Item
+                  onClick={() =>
+                    addToPanelStack({
+                      props: {
+                        content: 'Panel3',
+                      },
+                      renderPanel: PanelContainer,
+                      title: 'Panel 3',
+                    })
+                  }
+                  noTooltip
+                  title="plus"
+                  icon="plus"
+                />
+              </Toolbar>
+            </PanelHeader>
+          ) : (
+            <PanelPreferencesToolbar
+              onClose={removeFromPanelStack}
+              onSave={removeFromPanelStack}
+            />
+          )}
+          <div style={{ position: 'relative', height: '100%' }}>
+            <PanelStack2
+              css={css`
+                position: static;
+                & > div {
+                  margin: 0;
+                  border: none;
+                }
+              `}
+              onOpen={addToPanelStack}
+              onClose={removeFromPanelStack}
+              stack={currentPanelStack}
+              showPanelHeader={false}
+            />
+          </div>
         </Accordion.Item>
         <Accordion.Item title="Second Item">
           <PanelHeader current={2} total={3} onClickSettings={() => void 0}>
