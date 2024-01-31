5c488211e9a8d2d2f13c0180d292fd7b76ff1711
Wadjih Bencheikh <jw_bencheikh@esi.dz>
2024-01-31 13:43:54 +0100

    feat: render color picker dropdown in blueprintjs Popover
    Closes: https://github.com/zakodium-oss/react-science/issues/638

--- src/components/color-picker/color-picker-dropdown/ColorPickerDropdown.tsx
+++ src/components/color-picker/color-picker-dropdown/ColorPickerDropdown.tsx
@@ -1,18 +1,17 @@
+import { Popover, PopoverProps } from '@blueprintjs/core';
 import styled from '@emotion/styled';
-import { useRef } from 'react';
 
-import { useModifiedPopper } from '../../hooks/useModifiedPopper';
-import { useOnClickOutside } from '../../hooks/useOnClickOutside';
-import { useOnOff } from '../../hooks/useOnOff';
-import { Portal } from '../../root-layout/Portal';
 import FixedColorPreview from '../preview/FixedColorPreview';
 import { ColorPicker, ColorPickerProps } from '../react-color/ColorPicker';
 import * as colorHelper from '../react-color/helpers/color';
 
-type ColorPickerDropdownProps = Pick<
-  ColorPickerProps,
-  'color' | 'presetColors' | 'disableAlpha' | 'onChange' | 'onChangeComplete'
->;
+interface ColorPickerDropdownProps
+  extends Pick<
+    ColorPickerProps,
+    'color' | 'presetColors' | 'disableAlpha' | 'onChange' | 'onChangeComplete'
+  > {
+  popoverProps?: Omit<PopoverProps, 'content'>;
+}
 
 const ColorPickerRoot = styled.button`
   position: relative;
@@ -30,41 +29,22 @@ const ColorPickerPreview = styled.div`
 `;
 
 export function ColorPickerDropdown(props: ColorPickerDropdownProps) {
-  const { color, ...otherProps } = props;
-
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [isOpened, , close, toggle] = useOnOff(false);
+  const { color, popoverProps, ...otherProps } = props;
 
   const { hex } = colorHelper.toState(color || 'white');
 
-  const { setReferenceElement, setPopperElement, popperProps } =
-    useModifiedPopper<HTMLButtonElement>({
-      placement: 'bottom-start',
-      offset: 6,
-    });
-
-  useOnClickOutside(ref, close);
-
   return (
-    <>
-      <ColorPickerRoot type="button" ref={setReferenceElement} onClick={toggle}>
+    <Popover
+      targetProps={{ style: { width: '100%' } }}
+      content={<ColorPicker color={color} {...otherProps} />}
+      minimal
+      {...popoverProps}
+    >
+      <ColorPickerRoot type="button">
         <ColorPickerPreview>
           <FixedColorPreview color={hex} />
         </ColorPickerPreview>
       </ColorPickerRoot>
-      {isOpened && (
-        <Portal>
-          <div
-            ref={(div) => {
-              setPopperElement(div);
-              ref.current = div;
-            }}
-            {...popperProps}
-          >
-            <ColorPicker color={color} {...otherProps} />
-          </div>
-        </Portal>
-      )}
-    </>
+    </Popover>
   );
 }
