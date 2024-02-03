f44f17ff6021379f64f92846a0324dffe188b259
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2024-02-03 13:44:28 +0100

    refactor: add accordion scroll exmple

--- stories/components/accordion.stories.tsx
+++ stories/components/accordion.stories.tsx
@@ -25,7 +25,80 @@ export function Fixed() {
           This is the first content
         </Accordion.Item>
         <Accordion.Item title="Second Item">
-          This is the content of the second item
+          This is the content of the second item Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Error provident illum quidem quod dicta
+          sed aperiam ex iusto nesciunt culpa esse, quibusdam quam ea
+          praesentium vitae excepturi nulla aliquam maxime. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Consequuntur illo vel, quidem,
+          fuga autem sint debitis accusamus, aperiam totam nam itaque excepturi.
+          Fugit illo placeat dolorem quisquam vel dolores sed? Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. In aperiam excepturi facere
+          dolore? Ut ipsum deserunt atque soluta voluptas minus, in facere sequi
+          pariatur perferendis doloremque esse repellat exercitationem
+          excepturi. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Totam exercitationem sed libero deserunt repellat impedit, veniam,
+          laudantium consequuntur vero aspernatur odit. Ipsam delectus,
+          consequatur at eveniet dolorum accusantium dolore eligendi? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Esse, fugiat eum
+          obcaecati quod ex illum odit placeat, excepturi enim vero fuga ullam
+          corrupti sunt odio a blanditiis incidunt asperiores ipsam. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Labore corrupti est
+          numquam officiis, iusto minus ratione! Repudiandae iure ipsum laborum
+          itaque eligendi rerum nesciunt, reprehenderit vero eveniet velit
+          minima dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Facilis praesentium ipsum hic magnam ullam vel sunt. Eveniet non
+          minus libero soluta! Voluptatum autem id voluptate ex possimus est
+          soluta ab. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Maxime blanditiis suscipit fuga, sequi magnam fugiat sunt quibusdam!
+          Ut eos exercitationem sit nostrum est, aperiam adipisci nemo, in
+          placeat esse libero! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Mollitia, iste praesentium atque numquam unde
+          maiores eos dolorem quae eaque earum harum officiis, deserunt
+          provident! Praesentium quas deserunt iusto et alias. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Recusandae ea labore eveniet
+          praesentium itaque. Molestias mollitia architecto minima culpa
+          reiciendis doloribus. Repudiandae dolorem iusto, quibusdam consequatur
+          minima fugiat eum hic. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Voluptatibus maiores officia repellat cupiditate
+          libero, ad, exercitationem odit, assumenda ex aperiam tempore harum
+          delectus quidem amet dicta vitae commodi debitis. Fugiat? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Ad ipsam enim quis iusto
+          sit veniam esse id accusantium quos minima facilis commodi quibusdam,
+          quisquam odit ratione fugiat ducimus accusamus alias!ebitis accusamus,
+          aperiam totam nam itaque excepturi. Fugit illo placeat dolorem
+          quisquam vel dolores sed? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. In aperiam excepturi facere dolore? Ut ipsum
+          deserunt atque soluta voluptas minus, in facere sequi pariatur
+          perferendis doloremque esse repellat exercitationem excepturi. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Totam
+          exercitationem sed libero deserunt repellat impedit, veniam,
+          laudantium consequuntur vero aspernatur odit. Ipsam delectus,
+          consequatur at eveniet dolorum accusantium dolore eligendi? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Esse, fugiat eum
+          obcaecati quod ex illum odit placeat, excepturi enim vero fuga ullam
+          corrupti sunt odio a blanditiis incidunt asperiores ipsam. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Labore corrupti est
+          numquam officiis, iusto minus ratione! Repudiandae iure ipsum laborum
+          itaque eligendi rerum nesciunt, reprehenderit vero eveniet velit
+          minima dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Facilis praesentium ipsum hic magnam ullam vel sunt. Eveniet non
+          minus libero soluta! Voluptatum autem id voluptate ex possimus est
+          soluta ab. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Maxime blanditiis suscipit fuga, sequi magnam fugiat sunt quibusdam!
+          Ut eos exercitationem sit nostrum est, aperiam adipisci nemo, in
+          placeat esse libero! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Mollitia, iste praesentium atque numquam unde
+          maiores eos dolorem quae eaque earum harum officiis, deserunt
+          provident! Praesentium quas deserunt iusto et alias. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Recusandae ea labore eveniet
+          praesentium itaque. Molestias mollitia architecto minima culpa
+          reiciendis doloribus. Repudiandae dolorem iusto, quibusdam consequatur
+          minima fugiat eum hic. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Voluptatibus maiores officia repellat cupiditate
+          libero, ad, exercitationem odit, assumenda ex aperiam tempore harum
+          delectus quidem amet dicta vitae commodi debitis. Fugiat? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Ad ipsam enim quis iusto
+          sit veniam esse id accusantium quos minima facilis commodi quibus
         </Accordion.Item>
         <Accordion.Item title="With Toolbar">
           <Toolbar>
