100cd110536fa4264888ed69d173f07099f6fd12
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2024-02-09 17:25:39 +0100

    refactor: toobar-item font size

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -163,7 +163,7 @@ Toolbar.Item = function ToolbarItem(props: ToolbarItemProps) {
       intent={intent}
       style={{
         position: 'relative',
-        fontSize: '1.25em',
+        fontSize: '1.125em',
         width: 'fit-content',
         flex: 'none',
       }}
@@ -206,7 +206,7 @@ Toolbar.PopoverItem = function ToolbarPopoverItem(
       targetProps={{
         style: {
           position: 'relative',
-          fontSize: '1.25em',
+          fontSize: '1.125em',
           width: 'fit-content',
           height: 'fit-content',
           flex: 'none',
