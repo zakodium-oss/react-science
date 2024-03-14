c9125c876707cb520e8141d35a0f5e9bcc1fbd66
hamed-musallam <35760236+hamed-musallam@users.noreply.github.com>
2024-03-14 17:59:05 +0100

    feat: default selected value and custom text with useSelect (#681)
    BREAKING CHANGE: useSelect now takes `itemTextKey` or `getItemText` prop to choose what text should be rendered  in a Select option
    
    * feat: improve useSelect hook
    
    - It accepts a 'defaultSelectedItem' property.
    - You can specify which object serves as the label in two ways:
        - Using the 'itemLabelKey' property, which accepts only keys where the value type is string.
        - Using the 'itemLabelMap' function, which takes an item as a parameter and returns a string.
    
    * refactor: select storties
    
    * refactor: select properties
    
     - rename 'itemLabelKey' to 'itemTextKey'
     - rename 'itemLabelMap' to 'getItemText'

--- src/components/hooks/useSelect.tsx
+++ src/components/hooks/useSelect.tsx
@@ -2,9 +2,50 @@ import { MenuItem } from '@blueprintjs/core';
 import { ItemRenderer } from '@blueprintjs/select';
 import { useState } from 'react';
 
-export function useSelect<T extends { label: string }>() {
-  const [value, setValue] = useState<T | null>(null);
-  const itemRenderer = getItemRenderer(value);
+type FilterType<SourceType, Type> = Pick<
+  SourceType,
+  {
+    [K in keyof SourceType]: SourceType[K] extends Type ? K : never;
+  }[keyof SourceType]
+>;
+
+interface BaseOptions<T> {
+  itemTextKey: keyof FilterType<T, string>;
+  defaultSelectedItem?: T;
+}
+
+interface RenderOptions<T> {
+  getItemText: (item: T) => string;
+  defaultSelectedItem?: T;
+}
+
+type SelectOptions<T> = BaseOptions<T> | RenderOptions<T>;
+
+function isAccessLabelByKey<T>(
+  options: SelectOptions<T>,
+): options is BaseOptions<T> {
+  return 'itemTextKey' in options;
+}
+
+function getLabel<T>(item: T, options: SelectOptions<T>) {
+  const isAccessLByLabelKey = isAccessLabelByKey(options);
+
+  if (!item || (isAccessLByLabelKey && !options.itemTextKey)) {
+    return null;
+  }
+
+  if (isAccessLByLabelKey) {
+    return item[options.itemTextKey] as string;
+  }
+
+  return options.getItemText(item);
+}
+
+export function useSelect<T>(options: SelectOptions<T>) {
+  const { defaultSelectedItem = null } = options;
+
+  const [value, setValue] = useState<T | null>(defaultSelectedItem);
+  const itemRenderer = getItemRenderer(value as T, options);
   const onItemSelect = setValue;
   const popoverProps = {
     onOpened: (node: HTMLElement) => {
@@ -19,12 +60,16 @@ export function useSelect<T extends { label: string }>() {
     style: { display: 'inline-block' },
   };
   const itemPredicate = (query: string, item: T) => {
-    return item.label.toLowerCase().includes(query.toLowerCase());
+    const label = getLabel(item, options);
+    if (!label) return false;
+    return label.toLowerCase().includes(query.toLowerCase());
   };
   const itemListPredicate = (query: string, items: T[]) => {
-    return items.filter((item) =>
-      item.label.toLowerCase().includes(query.toLowerCase()),
-    );
+    return items.filter((item) => {
+      const label = getLabel(item, options);
+      if (!label) return false;
+      return label.toLowerCase().includes(query.toLowerCase());
+    });
   };
   return {
     value,
@@ -38,21 +83,27 @@ export function useSelect<T extends { label: string }>() {
   };
 }
 
-function getItemRenderer<T extends { label: string }>(value: T | null) {
+function getItemRenderer<T>(value: T, options: SelectOptions<T>) {
+  const selectedLabel = getLabel(value, options);
+
   const render: ItemRenderer<T> = (
-    { label },
+    item,
     { handleClick, handleFocus, modifiers, index },
-  ) => (
-    <MenuItem
-      active={modifiers.active}
-      disabled={modifiers.disabled}
-      selected={value?.label === label}
-      key={index}
-      onClick={handleClick}
-      onFocus={handleFocus}
-      roleStructure="listoption"
-      text={label}
-    />
-  );
+  ) => {
+    const label = getLabel(item, options);
+    return (
+      <MenuItem
+        active={modifiers.active}
+        disabled={modifiers.disabled}
+        selected={selectedLabel === label}
+        key={index}
+        onClick={handleClick}
+        onFocus={handleFocus}
+        roleStructure="listoption"
+        text={label}
+      />
+    );
+  };
+
   return render;
 }
