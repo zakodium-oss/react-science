24953c0b4853d477c2abf5b52d61b234b7c26caa
Michaël Zasso <targos@protonmail.com>
2024-06-24 16:58:11 +0200

    fix: remove dependency on d3-scale-chromatic types

--- src/components/color-picker/gradient-select/GradientSelect.tsx
+++ src/components/color-picker/gradient-select/GradientSelect.tsx
@@ -7,15 +7,23 @@ import { FaChevronDown } from 'react-icons/fa';
 
 import FixedGradientPreview from '../preview/FixedGradientPreview';
 
-export const fixedGradientScales = {
+export type GradientScaleName =
+  | 'turbo'
+  | 'viridis'
+  | 'inferno'
+  | 'magma'
+  | 'plasma';
+
+export const fixedGradientScales: Record<
+  GradientScaleName,
+  (t: number) => string
+> = {
   turbo: scaleChromatic.interpolateTurbo,
   viridis: scaleChromatic.interpolateViridis,
   inferno: scaleChromatic.interpolateInferno,
   magma: scaleChromatic.interpolateMagma,
   plasma: scaleChromatic.interpolatePlasma,
 };
-
-export type GradientScaleName = keyof typeof fixedGradientScales;
 
 const scaleOptions = Object.keys(fixedGradientScales) as GradientScaleName[];
 
