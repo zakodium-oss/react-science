45261cdb708e02f82a81ba75540101bca4c90def
Michaël Zasso <targos@protonmail.com>
2024-07-02 16:55:13 +0200

    chore: update ESLint to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import react from 'eslint-config-zakodium/react';
+import ts from 'eslint-config-zakodium/ts';
+import unicorn from 'eslint-config-zakodium/unicorn';
+
+export default [
+  {
+    ignores: [
+      '.storybook',
+      'build',
+      'coverage',
+      'dist',
+      'lib-esm',
+      'lib',
+      'node_modules',
+      'storybook-static',
+      'playwright/.cache',
+      'playwright-report*',
+    ],
+  },
+  ...ts,
+  ...react,
+  ...unicorn,
+  {
+    rules: {
+      'react/no-unknown-property': ['error', { ignore: ['css'] }],
+      '@typescript-eslint/no-dynamic-delete': 'off',
+    },
+  },
+];
