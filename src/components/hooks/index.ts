7557008529e0feefca7fdc80a1ad7a961eabd540
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2024-12-03 14:43:30 +0100

    fix: remove export in index

--- src/components/hooks/index.ts
+++ src/components/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useDoubleClick.js';
 export * from './useHashSearchParams.js';
-export * from './useModifiedPopper.js';
 export * from './useOnClickOutside.js';
 export * from './useOnOff.js';
 export * from './useToggle.js';
