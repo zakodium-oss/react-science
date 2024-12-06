69ae55020f534373129ce102176171fc04676a73
Michaël Zasso <targos@protonmail.com>
2024-12-06 16:54:06 +0100

    refactor: remove react-query from root layout
    BREAKING-CHANGE: `@tanstack/react-query`'s QueryClientProvider was removed from the `RootLayout` component.
    You'll have to provide your own client if you relied on it to use react-query in your application.

--- src/components/root-layout/root_layout.tsx
+++ src/components/root-layout/root_layout.tsx
@@ -1,12 +1,10 @@
 import { BlueprintProvider, FocusStyleManager } from '@blueprintjs/core';
-import { QueryClientProvider } from '@tanstack/react-query';
 import type { CSSProperties, ReactNode } from 'react';
 import { useCallback, useState } from 'react';
 
 import { AccordionProvider } from '../accordion/index.js';
 
 import { CustomDivPreflight } from './css-reset/customPreflight.js';
-import { queryClient } from './query_client.js';
 import { RootLayoutProvider } from './root_layout_context.provider.js';
 
 FocusStyleManager.onlyShowFocusOnTabs();
@@ -48,9 +46,7 @@ export function RootLayout(props: RootLayoutProps) {
         {...(rootRef ? { portalContainer: rootRef } : undefined)}
       >
         <RootLayoutProvider innerRef={rootRef}>
-          <QueryClientProvider client={queryClient}>
-            <AccordionProvider>{props.children}</AccordionProvider>
-          </QueryClientProvider>
+          <AccordionProvider>{props.children}</AccordionProvider>
         </RootLayoutProvider>
       </BlueprintProvider>
     </CustomDivPreflight>
