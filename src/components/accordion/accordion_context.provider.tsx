f86807c2243898ce078a471221fb3a403e5c66ee
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2024-12-16 16:51:59 +0100

    chore: move immer to devDependencies (#814)
    Closes: https://github.com/zakodium-oss/react-science/issues/808

--- src/components/accordion/accordion_context.provider.tsx
+++ src/components/accordion/accordion_context.provider.tsx
@@ -1,53 +1,91 @@
 import type { ReactNode } from 'react';
 import { useMemo, useReducer } from 'react';
+import { match } from 'ts-pattern';
 
 import type { ContextType } from './accordion_context.js';
 import { accordionContext } from './accordion_context.js';
-import { reducer } from './accordion_context.state.js';
+import type {
+  AccordionItemState,
+  ActionType,
+} from './accordion_context.utils.js';
+import {
+  changeItem,
+  clearItem,
+  removeItem,
+  toggleItem,
+} from './accordion_context.utils.js';
 
-export function AccordionProvider(props: {
+interface AccordionProviderProps {
   children: ReactNode;
   unmountChildren?: boolean;
-}) {
-  const { unmountChildren = false } = props;
-  const [state, dispatch] = useReducer(reducer, {
-    items: [],
-    unmountChildren,
-  });
+}
 
-  const utils = useMemo(
-    () => ({
-      change: (title: string, isOpen: boolean) => {
-        return dispatch({
-          type: 'CHANGE_ITEM',
-          payload: { isOpen, title },
-        });
-      },
-      clear: (except: string) => {
-        return dispatch({ type: 'CLEAR', payload: except });
-      },
-      toggle: (title: string) => {
-        return dispatch({ type: 'TOGGLE', payload: title });
-      },
-      remove: (title: string) => {
-        return dispatch({ type: 'REMOVE_ITEM', payload: title });
-      },
+export type AccordionActions =
+  | ActionType<'CHANGE', { title: string; isOpen: boolean }>
+  | ActionType<'CLEAR', string>
+  | ActionType<'TOGGLE', string>
+  | ActionType<'REMOVE', string>
+  | ActionType<'CREATE', AccordionItemState>;
+
+function reducer(
+  previous: AccordionItemState[],
+  action: AccordionActions,
+): AccordionItemState[] {
+  return match(action)
+    .with({ type: 'CHANGE' }, ({ payload }) => {
+      return changeItem(previous, payload);
+    })
+    .with({ type: 'CLEAR' }, ({ payload }) => {
+      return clearItem(previous, payload);
+    })
+    .with({ type: 'TOGGLE' }, ({ payload }) => {
+      return toggleItem(previous, payload);
+    })
+    .with({ type: 'REMOVE' }, ({ payload }) => {
+      return removeItem(previous, payload);
+    })
+    .with({ type: 'CREATE' }, ({ payload }) => {
+      return [...previous, payload];
+    })
+    .exhaustive();
+}
+
+export function AccordionProvider(props: AccordionProviderProps) {
+  const { unmountChildren = false, children } = props;
+  const [items, dispatch] = useReducer(reducer, []);
+
+  const utils = useMemo<ContextType[1]>(() => {
+    return {
+      change: (title: string, isOpen: boolean) =>
+        dispatch({ type: 'CHANGE', payload: { title, isOpen } }),
+      clear: (except: string) => dispatch({ type: 'CLEAR', payload: except }),
+      toggle: (title: string) => dispatch({ type: 'TOGGLE', payload: title }),
+      remove: (title: string) => dispatch({ type: 'REMOVE', payload: title }),
       create: (title: string, defaultOpened?: boolean) => {
-        const item = { title, isOpen: defaultOpened || false };
-        dispatch({ type: 'CREATE_ITEM', payload: item });
+        const item: AccordionItemState = {
+          title,
+          isOpen: defaultOpened || false,
+        };
+
+        dispatch({ type: 'CREATE', payload: item });
         return item;
       },
-    }),
-    [dispatch],
-  );
+    };
+  }, []);
 
-  const value = useMemo<ContextType>(() => {
-    return [{ items: state.items, unmountChildren }, utils];
-  }, [state.items, utils, unmountChildren]);
+  const contextValue = useMemo<ContextType>(() => {
+    return [
+      {
+        items,
+        unmountChildren,
+      },
+      utils,
+    ];
+  }, [items, unmountChildren, utils]);
 
   return (
-    <accordionContext.Provider value={value}>
-      {props.children}
+    <accordionContext.Provider value={contextValue}>
+      {children}
     </accordionContext.Provider>
   );
 }
