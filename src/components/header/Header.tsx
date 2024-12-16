11cd2a20532604bba7bf8a2f51f97a1d664985f9
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2024-12-16 16:02:06 +0100

    refactor(Header): change css to use styled (#825)
    Refs: https://github.com/zakodium-oss/react-science/issues/806

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import type { ReactElement } from 'react';
 
 import type { ToolbarProps } from '../toolbar/Toolbar.js';
@@ -10,24 +10,20 @@ interface HeaderProps {
   children: [ReactProps, ReactProps];
 }
 
-const styles = {
-  header: css({
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-
-    justifyContent: 'space-between',
-    borderBottom: '1px solid rgb(247, 247, 247);',
-
-    boxShadow: 'rgb(255, 255, 255) 0px 1px 0px 0px inset',
-  }),
-};
+const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 1px solid rgb(247, 247, 247);
+  box-shadow: rgb(255, 255, 255) 0 1px 0 0 inset;
+`;
 
 export function Header(props: HeaderProps) {
   return (
-    <div css={styles.header}>
+    <HeaderContainer>
       <div>{props.children[0]}</div>
       <div>{props.children[1]}</div>
-    </div>
+    </HeaderContainer>
   );
 }
