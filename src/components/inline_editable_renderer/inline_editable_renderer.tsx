4848f7073a9913c82eecd47196da8acdfe95a375
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2025-01-21 15:24:04 +0100

    feat: create InlineEditable component (#846)

---
+++ src/components/inline_editable_renderer/inline_editable_renderer.tsx
@@ -0,0 +1,86 @@
+import { Colors } from '@blueprintjs/core';
+import styled from '@emotion/styled';
+import type { KeyboardEvent, ReactNode } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+
+export interface InlineRendererEditableProps<T extends HTMLElement> {
+  ref: (node: T | null) => void;
+  /**
+   * Function to exit the editable state and display the children content.
+   */
+  exit: () => void;
+  onKeyDown: (event: KeyboardEvent<HTMLInputElement>) => void;
+}
+
+export interface InlineEditableProps<T extends HTMLElement> {
+  renderEditable: (props: InlineRendererEditableProps<T>) => ReactNode;
+  children: ReactNode;
+}
+
+export const InlineEditableInput = styled.input`
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 0 1px 1px ${Colors.GRAY1};
+  position: absolute;
+  outline: none;
+  inset: 0;
+`;
+
+const Container = styled.div`
+  min-width: 100%;
+  width: 100%;
+  min-height: 21px;
+
+  :focus,
+  :hover {
+    box-shadow: 0 0 1px 1px ${Colors.GRAY1};
+  }
+`;
+
+/**
+ * The `InlineEditable` component allows for inline editing of its content.
+ * It renders a component with `renderEditable` when focused or clicked
+ * and toggles back to the original content when the input loses focus.
+ */
+export function InlineEditable<T extends HTMLElement>(
+  props: InlineEditableProps<T>,
+) {
+  const { children, renderEditable } = props;
+  const [isInputRendered, setIsInputRendered] = useState(false);
+
+  const toggle = useCallback(() => {
+    return setIsInputRendered((old) => !old);
+  }, []);
+
+  const renderEditableProps = useMemo<InlineRendererEditableProps<T>>(() => {
+    return {
+      isRendered: isInputRendered,
+      onKeyDown: (event) => {
+        if (event.key === 'Enter') {
+          setIsInputRendered(false);
+        }
+      },
+      ref: (node) => {
+        if (!node) return;
+        node.focus();
+      },
+      exit: () => setIsInputRendered(false),
+    };
+  }, [isInputRendered]);
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <div style={{ visibility: isInputRendered ? 'visible' : 'hidden' }}>
+        {renderEditable(renderEditableProps)}
+      </div>
+
+      <Container
+        tabIndex={isInputRendered ? -1 : 0}
+        onFocus={() => setIsInputRendered(true)}
+        onClick={toggle}
+      >
+        {children}
+      </Container>
+    </div>
+  );
+}
---
+++ stories/components/editable.stories.tsx
@@ -0,0 +1,203 @@
+import { Button } from '@blueprintjs/core';
+import type { StoryObj } from '@storybook/react';
+import { useCallback, useMemo, useState } from 'react';
+
+import { createTableColumnHelper, Table } from '../../src/components/index.js';
+import {
+  InlineEditable as InlineEditableComponent,
+  InlineEditableInput,
+} from '../../src/components/inline_editable_renderer/index.js';
+
+export default {
+  title: 'Components / InlineEditableComponent',
+};
+
+interface TableData {
+  id: number;
+  label: string;
+  field: string;
+  format: string;
+  visible: boolean;
+}
+
+const helper = createTableColumnHelper<TableData>();
+
+const data: TableData[] = [
+  { label: 'Name', field: 'info.name', format: '', visible: true },
+  {
+    label: 'Number of scans',
+    field: 'info.numberOfScans',
+    format: '0',
+    visible: true,
+  },
+  {
+    label: 'Pulse sequence',
+    field: 'info.pulseSequence',
+    format: '',
+    visible: true,
+  },
+  {
+    label: 'Frequency',
+    field: 'meta..DIGITSERRE',
+    format: '0',
+    visible: false,
+  },
+].map((item, index) => ({ id: index, ...item }));
+
+export function InsideTable() {
+  const [state, setState] = useState(data);
+
+  const deleteIndex = useCallback((index: number) => {
+    return setState((prev) => prev.filter((_, i) => i !== index));
+  }, []);
+
+  const changeValue = useCallback(
+    (rowIndex: number, key: string, value: string) => {
+      setState((prev) => {
+        const element = prev.at(rowIndex);
+
+        if (!element) {
+          return prev;
+        }
+
+        return prev.map((item, index) => {
+          if (index === rowIndex) {
+            return { ...element, [key]: value };
+          }
+
+          return item;
+        });
+      });
+    },
+    [],
+  );
+
+  const columns = useMemo(() => {
+    return [
+      helper.accessor('id', { header: '#' }),
+      helper.accessor('label', {
+        header: 'Label',
+        cell: ({ getValue, row: { index } }) => (
+          <InlineEditableComponent
+            renderEditable={({ ref, exit, onKeyDown }) => (
+              <InlineEditableInput
+                ref={ref}
+                onKeyDown={onKeyDown}
+                defaultValue={getValue()}
+                onBlur={(event) => {
+                  exit();
+                  changeValue(index, 'label', event.currentTarget.value);
+                }}
+              />
+            )}
+          >
+            {getValue()}
+          </InlineEditableComponent>
+        ),
+      }),
+      helper.accessor('field', {
+        header: 'Field',
+        cell: ({ getValue, row: { index } }) => (
+          <InlineEditableComponent
+            renderEditable={({ ref, exit, onKeyDown }) => (
+              <InlineEditableInput
+                ref={ref}
+                onKeyDown={onKeyDown}
+                defaultValue={getValue()}
+                onBlur={(event) => {
+                  exit();
+                  changeValue(index, 'field', event.currentTarget.value);
+                }}
+              />
+            )}
+          >
+            {getValue()}
+          </InlineEditableComponent>
+        ),
+      }),
+      helper.accessor('format', {
+        header: 'Format',
+        cell: ({ getValue, row: { index } }) => (
+          <InlineEditableComponent
+            renderEditable={({ ref, exit, onKeyDown }) => (
+              <InlineEditableInput
+                ref={ref}
+                onKeyDown={onKeyDown}
+                defaultValue={getValue()}
+                onBlur={(event) => {
+                  exit();
+                  changeValue(index, 'format', event.currentTarget.value);
+                }}
+              />
+            )}
+          >
+            {getValue()}
+          </InlineEditableComponent>
+        ),
+      }),
+      helper.accessor('visible', { header: 'Visible' }),
+      helper.display({
+        header: ' ',
+        cell: ({ row: { index } }) => {
+          return (
+            <Button
+              type="button"
+              icon="trash"
+              tabIndex={-1}
+              onClick={() => deleteIndex(index)}
+            />
+          );
+        },
+      }),
+    ];
+  }, [changeValue, deleteIndex]);
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 5,
+      }}
+    >
+      <div>
+        <Table<TableData>
+          data={state}
+          columns={columns}
+          estimatedRowHeight={() => 50}
+        />
+      </div>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </div>
+  );
+}
+
+export const InlineEditable = {
+  render: () => {
+    const [state, setState] = useState('Hello, World!');
+
+    return (
+      <div style={{ width: 100 }}>
+        <span>State: {state}</span>
+        <InlineEditableComponent
+          renderEditable={({ ref, exit, onKeyDown }) => (
+            <InlineEditableInput
+              ref={ref}
+              onKeyDown={onKeyDown}
+              defaultValue={state}
+              onBlur={(event) => {
+                exit();
+
+                setState(event.currentTarget.value);
+              }}
+            />
+          )}
+        >
+          {state}
+        </InlineEditableComponent>
+      </div>
+    );
+  },
+} satisfies StoryObj;
