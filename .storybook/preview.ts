5c539fc3e2d27f47913300d7a5d3b014191dcfde
Daniel Kostro <kostro.d@gmail.com>
2025-01-24 11:13:46 +0100

    chore: import blueprint-select css in storybook and app setups

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,7 @@ import { RootLayoutDecorator } from '../stories/utils.js';
 import '../styles/storybook-globals.css';
 import '../styles/preflight.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/select/lib/css/blueprint-select.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 const preview: Preview = {
