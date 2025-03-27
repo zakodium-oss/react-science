9a4a5c1b30afaccb7cac3c07ba53100ca9f36ad2
Michaël Zasso <targos@protonmail.com>
2025-03-27 13:26:50 +0100

    fixup! feat: improve fullscreen error handling

--- stories/components/fullscreen.stories.tsx
+++ stories/components/fullscreen.stories.tsx
@@ -14,10 +14,14 @@ export default {
 export function Basic() {
   return (
     <FullScreenProvider
-      onToggleError={(error, step) =>
+      onToggleError={(error, step) => {
+        // eslint-disable-next-line no-console
+        console.error(error, step);
         // eslint-disable-next-line no-alert
-        window.alert(`Fullscreen error during "${step}"`)
-      }
+        window.alert(
+          `Fullscreen error during "${step}". See the browser console for details.`,
+        );
+      }}
     >
       {(page1) => (
         <FullPageRoot ref={page1} color="green">
