8ccdc9e519052a01d8822d8edeac7ed98b3f1f38
Michaël Zasso <targos@protonmail.com>
2025-05-21 16:30:56 +0200

    feat(Table): add support for `thStyle` in column meta

--- src/components/table/index.ts
+++ src/components/table/index.ts
@@ -1,4 +1,18 @@
+import type { RowData } from '@tanstack/react-table';
+import type { CSSProperties } from 'react';
+
 export * from './table_root.js';
 export * from './table_utils.js';
 export * from './table_row.js';
 export * from './reorder_rows/index.js';
+
+declare module '@tanstack/react-table' {
+  // Declaration merging
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface ColumnMeta<TData extends RowData, TValue> {
+    /**
+     * Merged into the `style` prop of the default-rendered `<th>` element.
+     */
+    thStyle?: CSSProperties;
+  }
+}
