f16e18b817d48c996393bf620833fbb0594accb7
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2025-06-10 11:03:42 +0200

    refactor: remove unused space

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,14 +3,12 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   core: {},
-
   framework: {
     name: '@storybook/react-vite',
     options: {
       strictMode: false,
     },
   },
-
   addons: ['@storybook/addon-docs'],
 };
 
