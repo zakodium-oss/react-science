f2b3d5b89de2ccb728b557efd85bea33d63284bd
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2025-06-15 13:19:11 +0200

    feat: add DOI component (#921)
    Closes: https://github.com/zakodium-oss/react-science/issues/916

---
+++ src/components/doi/doi.tsx
@@ -0,0 +1,35 @@
+import { match } from 'ts-pattern';
+
+import { Logo } from './Logo.js';
+import { normalizeDOI } from './utils.js';
+
+type DOISize = 'small' | 'medium' | 'large';
+
+export interface DOIProps {
+  value: string;
+  size?: DOISize;
+}
+
+export function DOI(props: DOIProps) {
+  const { value, size = 'small' } = props;
+  const normalizedValue = normalizeDOI(value);
+
+  return (
+    <a
+      title={normalizedValue}
+      href={`https://doi.org/${normalizedValue}`}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Logo size={getDOISize(size)} />
+    </a>
+  );
+}
+
+function getDOISize(size: DOISize): string {
+  return match(size)
+    .with('small', () => '1.25rem')
+    .with('medium', () => '2.5rem')
+    .with('large', () => '3.75rem')
+    .exhaustive();
+}
