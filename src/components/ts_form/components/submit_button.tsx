8d54e6f10b35ee18bc4074bd6810f418f84be724
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2025-08-20 15:47:59 +0200

    feat(TanstackForm): disable submit button when form is not dirty

--- src/components/ts_form/components/submit_button.tsx
+++ src/components/ts_form/components/submit_button.tsx
@@ -9,16 +9,17 @@ type SubmitButtonProps = ButtonProps;
 export function SubmitButton(props: SubmitButtonProps) {
   const form = useFormContext();
 
-  const [isSubmitting, errors] = useStore(form.store, (state) => [
+  const [isSubmitting, errors, isDirty] = useStore(form.store, (state) => [
     state.isSubmitting,
     state.errors,
+    state.isDirty,
   ]);
 
   return (
     <Button
       {...props}
       type="submit"
-      disabled={isSubmitting || errors.length > 0}
+      disabled={isSubmitting || errors.length > 0 || !isDirty}
     />
   );
 }
