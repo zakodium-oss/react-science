c769f7334d84de84259726657746af1448deca89
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2025-08-20 15:25:59 +0200

    feat(TanstackForm): allow to focus input by clicking on the label

--- src/components/ts_form/components/input.tsx
+++ src/components/ts_form/components/input.tsx
@@ -2,7 +2,7 @@ import type { InputGroupProps } from '@blueprintjs/core';
 import { Classes, FormGroup, InputGroup } from '@blueprintjs/core';
 import styled from '@emotion/styled';
 import type { ChangeEvent } from 'react';
-import { useMemo } from 'react';
+import { useId, useMemo } from 'react';
 
 import { useFieldContext } from '../context/use_ts_form.js';
 
@@ -19,6 +19,7 @@ const StyledInput = styled(InputGroup)<{ error: boolean }>`
 export function Input(props: InputProps) {
   const { label, required, ...rest } = props;
   const field = useFieldContext<string>();
+  const id = useId();
 
   function onChange(event: ChangeEvent<HTMLInputElement>) {
     return field.handleChange(event.target.value);
@@ -34,6 +35,7 @@ export function Input(props: InputProps) {
       helperText={error}
       label={label}
       labelInfo={required && <span style={{ color: 'red' }}>*</span>}
+      labelFor={`input-${field.name}-${id}`}
       intent={error !== undefined ? 'danger' : undefined}
     >
       <StyledInput
@@ -43,6 +45,7 @@ export function Input(props: InputProps) {
         onChange={onChange}
         error={error !== undefined}
         required={required}
+        id={`input-${field.name}-${id}`}
       />
     </FormGroup>
   );
