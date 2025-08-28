ef9e4818c52a75b2e21be464b149fbfb272b5f96
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2025-08-28 14:18:45 +0200

    feat(TanstackForm): re-activate Submit button everytime

--- src/components/form/components/submit_button.tsx
+++ src/components/form/components/submit_button.tsx
@@ -8,18 +8,7 @@ type SubmitButtonProps = ButtonProps;
 
 export function SubmitButton(props: SubmitButtonProps) {
   const form = useFormContext();
+  const [isSubmitting] = useStore(form.store, (state) => [state.isSubmitting]);
 
-  const [isSubmitting, errors, isDirty] = useStore(form.store, (state) => [
-    state.isSubmitting,
-    state.errors,
-    state.isDirty,
-  ]);
-
-  return (
-    <Button
-      {...props}
-      type="submit"
-      disabled={isSubmitting || errors.length > 0 || !isDirty}
-    />
-  );
+  return <Button {...props} type="submit" disabled={isSubmitting} />;
 }
