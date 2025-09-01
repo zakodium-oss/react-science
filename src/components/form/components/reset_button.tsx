25d89a38531d074182226657120c204480842b45
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2025-09-01 15:26:28 +0200

    refactor(TanstackForm): use Button with intent instead of specific button

--- src/components/form/components/reset_button.tsx
+++ src/components/form/components/reset_button.tsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled';
 import type { ReactNode } from 'react';
 
 import { Button } from '../../button/index.js';
@@ -8,18 +7,13 @@ interface ResetButtonProps {
   children: ReactNode;
 }
 
-const StyledResetButton = styled(Button)`
-  background-color: #dd3f3f !important;
-  color: white !important;
-`;
-
 export function ResetButton(props: ResetButtonProps) {
   const { children } = props;
   const form = useFormContext();
 
   return (
-    <StyledResetButton type="reset" onClick={() => form.reset()}>
+    <Button intent="danger" type="reset" onClick={() => form.reset()}>
       {children}
-    </StyledResetButton>
+    </Button>
   );
 }
