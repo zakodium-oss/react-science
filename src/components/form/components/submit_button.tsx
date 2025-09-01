32eb7e053e51062d6909391d97bcbbd35ce8a1ae
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2025-09-01 15:16:07 +0200

    feat(TanstackForm): set base intent for submit button to primary

--- src/components/form/components/submit_button.tsx
+++ src/components/form/components/submit_button.tsx
@@ -7,8 +7,17 @@ import { useFormContext } from '../context/use_ts_form.js';
 type SubmitButtonProps = ButtonProps;
 
 export function SubmitButton(props: SubmitButtonProps) {
+  const { intent, ...otherProps } = props;
+
   const form = useFormContext();
   const [isSubmitting] = useStore(form.store, (state) => [state.isSubmitting]);
 
-  return <Button {...props} type="submit" disabled={isSubmitting} />;
+  return (
+    <Button
+      {...otherProps}
+      intent={intent ?? 'primary'}
+      type="submit"
+      disabled={isSubmitting}
+    />
+  );
 }
