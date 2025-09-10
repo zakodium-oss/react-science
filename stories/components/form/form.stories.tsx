60c0eaafb0e53901c86762bcdd89cb7e7da88104
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2025-09-10 11:42:59 +0200

    docs(TanstackForm): add blur validation

--- stories/components/form/form.stories.tsx
+++ stories/components/form/form.stories.tsx
@@ -159,3 +159,39 @@ export function Example() {
     </Form>
   );
 }
+
+const blurSchema = z.object({
+  name: z.string().min(5, 'Name must be at least 5 characters long'),
+});
+
+export function BlurValidation() {
+  const form = useForm({
+    validationLogic: revalidateLogic({ modeAfterSubmission: 'blur' }),
+    validators: { onDynamic: blurSchema },
+    defaultValues: {
+      name: '',
+    },
+    onSubmit: ({ value }) => {
+      return blurSchema.parse(value);
+    },
+  });
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    void form.handleSubmit();
+  }
+
+  return (
+    <form noValidate onSubmit={handleSubmit}>
+      <form.AppField name="name">
+        {(field) => (
+          <field.Input label="Name" required placeholder="min 5 length" />
+        )}
+      </form.AppField>
+
+      <form.AppForm>
+        <form.SubmitButton>Submit</form.SubmitButton>
+      </form.AppForm>
+    </form>
+  );
+}
