3eed58f1c2996405f8f8a74af20da4d5a3729091
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2026-01-30 16:28:09 +0100

    Update src/components/form/components/field_groups/svg_line_style.schema.ts
    Co-authored-by: Michaël Zasso <targos@protonmail.com>

--- src/components/form/components/field_groups/svg_line_style.schema.ts
+++ src/components/form/components/field_groups/svg_line_style.schema.ts
@@ -7,7 +7,7 @@ export const svgLineStyleFieldsSchema = z.object({
     .string()
     .trim()
     .regex(/^#[0-9a-fA-F]{6}$/, {
-      message: 'Color must be hexadecimal and should not contains alpha',
+      message: 'Color must be hexadecimal and should not contain alpha',
     }),
   strokeOpacity: z.coerce.number<string>().min(0).max(1),
   strokeWidth: z.coerce.number<string>(),
