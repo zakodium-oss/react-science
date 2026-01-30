27c190a164415be0a53aaacadc6a6c9cac7d05d5
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2026-01-30 10:44:32 +0100

    feat: improves layout

--- src/components/form/components/field_groups/svg_text_style.tsx
+++ src/components/form/components/field_groups/svg_text_style.tsx
@@ -1,11 +1,12 @@
+import { Checkbox } from '@blueprintjs/core';
 import styled from '@emotion/styled';
+import type { ReactNode } from 'react';
 import { memo } from 'react';
 import type { z } from 'zod';
 
-import type { ButtonProps } from '../../../button/index.js';
-import { Button } from '../../../button/index.js';
 import { SVGStyledText } from '../../../svg/index.js';
 import { withFieldGroup } from '../../context/use_ts_form.js';
+import { FormGroup } from '../input_groups/form_group.tsx';
 
 import { Fieldset, Legend } from './fieldset.tsx';
 import { svgTextStyleFieldsSchema } from './svg_text_style_fields.schema.ts';
@@ -13,7 +14,7 @@ import { svgTextStyleFieldsSchema } from './svg_text_style_fields.schema.ts';
 const TextStyleSwitchContainer = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 0.5rem;
+  gap: 1rem;
 `;
 
 type SvgTextStyleFields = z.input<typeof svgTextStyleFieldsSchema>;
@@ -27,12 +28,23 @@ const defaultValues: SvgTextStyleFields = {
   fontWeight: 'normal',
 };
 
+interface SVGTextStyleFieldsProps {
+  label: string;
+  placeholder?: string;
+}
+
+const inferSVGTextStyleFieldsProps: SVGTextStyleFieldsProps = {
+  label: '',
+};
+
 export const FieldGroupSVGTextStyleFields = withFieldGroup({
   defaultValues,
-  props: {
-    label: '',
-  },
-  render: function SVGTextStyleFields({ group, label }) {
+  props: inferSVGTextStyleFieldsProps,
+  render: function SVGTextStyleFields({
+    group,
+    label,
+    placeholder = 'Placeholder',
+  }) {
     return (
       <Fieldset>
         <Legend>{label}</Legend>
@@ -42,66 +54,50 @@ export const FieldGroupSVGTextStyleFields = withFieldGroup({
         <group.AppField name="fontSize">
           {(field) => <field.NumericInput label="Font size" />}
         </group.AppField>
-        <TextStyleSwitchContainer>
-          <group.Field name="fontWeight">
-            {(field) => (
-              <TextStyleSwitch
-                icon="bold"
-                tooltip="Bold"
-                active={field.state.value === 'bold'}
-                onToggle={() =>
-                  field.setValue((currentValue) =>
-                    currentValue === 'normal' ? 'bold' : 'normal',
-                  )
-                }
-              />
-            )}
-          </group.Field>
-          <group.Field name="fontStyle">
-            {(field) => (
-              <TextStyleSwitch
-                icon="italic"
-                tooltip="Italic"
-                active={field.state.value === 'italic'}
-                onToggle={() =>
-                  field.setValue((currentValue) =>
-                    currentValue === 'normal' ? 'italic' : 'normal',
-                  )
-                }
-              />
-            )}
-          </group.Field>
-        </TextStyleSwitchContainer>
+        <FormGroup name="" label="Font modifiers">
+          <TextStyleSwitchContainer>
+            <group.Field name="fontWeight">
+              {(field) => (
+                <Checkbox
+                  labelElement={<BoldLabel>Bold</BoldLabel>}
+                  name={field.name}
+                  checked={fontWeightToBoolean(field.state.value)}
+                  onChange={(e) =>
+                    field.handleChange(booleanToFontWeight(e.target.checked))
+                  }
+                  onBlur={field.handleBlur}
+                />
+              )}
+            </group.Field>
+            <group.Field name="fontStyle">
+              {(field) => (
+                <Checkbox
+                  labelElement={<ItalicLabel>Italic</ItalicLabel>}
+                  name={field.name}
+                  checked={fontStyleToBoolean(field.state.value)}
+                  onChange={(e) =>
+                    field.handleChange(booleanToFontStyle(e.target.checked))
+                  }
+                  onBlur={field.handleBlur}
+                />
+              )}
+            </group.Field>
+          </TextStyleSwitchContainer>
+        </FormGroup>
 
-        <group.Subscribe selector={(state) => state.values}>
-          {(values) => <TextStyleFieldPreview {...values} />}
-        </group.Subscribe>
+        <FormGroup name="" label="Preview">
+          <group.Subscribe selector={(state) => state.values}>
+            {(values) => (
+              <TextStyleFieldPreview {...values}>
+                {placeholder}
+              </TextStyleFieldPreview>
+            )}
+          </group.Subscribe>
+        </FormGroup>
       </Fieldset>
     );
   },
 });
-
-interface TextStyleSwitchProps {
-  icon: ButtonProps['icon'];
-  tooltip: Exclude<ButtonProps['tooltipProps'], undefined>['content'];
-  active: boolean;
-  onToggle: () => void;
-}
-
-function TextStyleSwitch(props: TextStyleSwitchProps) {
-  const { icon, tooltip, active, onToggle } = props;
-  return (
-    <Button
-      icon={icon}
-      fill={false}
-      active={active}
-      onClick={onToggle}
-      tooltipProps={{
-        content: tooltip,
-      }}
-    />
-  );
-}
 
 const TextStyleFieldPreviewContainer = styled.div`
   display: flex;
@@ -109,8 +105,12 @@ const TextStyleFieldPreviewContainer = styled.div`
   min-height: 30px;
 `;
 
+interface TextStyleFieldPreviewProps extends SvgTextStyleFields {
+  children?: ReactNode;
+}
+
 const TextStyleFieldPreview = memo(function TextStyleFieldPreview(
-  props: SvgTextStyleFields,
+  props: TextStyleFieldPreviewProps,
 ) {
   const parsedValues = svgTextStyleFieldsSchema.parse(props);
   const svgHeight = Math.round(parsedValues.fontSize * 1.5);
@@ -125,9 +125,36 @@ const TextStyleFieldPreview = memo(function TextStyleFieldPreview(
           y={textY}
           {...parsedValues}
         >
-          Preview
+          {props.children}
         </SVGStyledText>
       </svg>
     </TextStyleFieldPreviewContainer>
   );
 });
+
+const BoldLabel = styled.span`
+  font-weight: bold;
+`;
+const ItalicLabel = styled.span`
+  font-style: italic;
+`;
+
+function fontWeightToBoolean(
+  weight: SvgTextStyleFields['fontWeight'],
+): boolean {
+  return weight === 'bold';
+}
+
+function booleanToFontWeight(
+  weight: boolean,
+): SvgTextStyleFields['fontWeight'] {
+  return weight ? 'bold' : 'normal';
+}
+
+function fontStyleToBoolean(weight: SvgTextStyleFields['fontStyle']): boolean {
+  return weight === 'italic';
+}
+
+function booleanToFontStyle(weight: boolean): SvgTextStyleFields['fontStyle'] {
+  return weight ? 'italic' : 'normal';
+}
