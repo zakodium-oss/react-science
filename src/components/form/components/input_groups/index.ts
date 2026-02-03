1f9788c613425a5553d62510d61010048992413b
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2026-02-03 12:20:36 +0100

    feat: export tanstack forms (#970)

---
+++ src/components/form/components/input_groups/index.ts
@@ -0,0 +1,4 @@
+export * from './form.js';
+export * from './form_group.js';
+
+export { useFormContext as useLayoutFormContext } from './form_context.js';
--- src/components/index.ts
+++ src/components/index.ts
@@ -16,6 +16,7 @@ export * from './table/index.js';
 export * from './toolbar/index.js';
 export * from './form/context/use_ts_form.js';
 export * from './form/components/field_groups/index.js';
+export * from './form/components/input_groups/index.js';
 export * from './activity_bar/index.js';
 export * from './activity_panel/index.js';
 export * from './utils/index.js';
