438a8dfbeab0dce01954e6024cd7927c1be25d2e
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2026-02-05 15:04:04 +0100

    feat(TanstackForm): export multiple input to be used in projects
    * Change name of Input to avoid collision with other components. (Added a prefix)
    * Export every input to outside of the project
    * Export Section to be used outside of the project
    
    BREAKING CHANGE: Input component from Tanstack has changed name. They are now prefixed with `Input`

---
+++ src/components/form/components/input/index.tsx
@@ -0,0 +1,8 @@
+export { Checkbox as CheckboxInput } from './checkbox.js';
+export { ColorPicker as ColorPickerInput } from './color_picker.js';
+export { Input } from './input.js';
+export { NumericInput } from './numeric_input.js';
+export { ResetButton } from './reset_button.js';
+export { Select as SelectInput } from './select.js';
+export { SubmitButton } from './submit_button.js';
+export { Switch as SwitchInput } from './switch.js';
