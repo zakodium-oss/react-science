56bbf46acd19bd608314950a1ca02f0c349a88c6
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2026-02-05 16:04:38 +0100

    feat(TanstackForm): export multiple input type to be used in projects (#974)

---
+++ src/components/form/components/input/index.tsx
@@ -0,0 +1,8 @@
+export { type Checkbox as _Checkbox } from './checkbox.js';
+export { type ColorPicker as _ColorPicker } from './color_picker.js';
+export { type Input as _Input } from './input.js';
+export { type NumericInput as _NumericInput } from './numeric_input.js';
+export { type ResetButton as _ResetButton } from './reset_button.js';
+export { type Select as _Select } from './select.js';
+export { type SubmitButton as _SubmitButton } from './submit_button.js';
+export { type Switch as _Switch } from './switch.js';
---
+++ src/components/form/components/layout/index.tsx
@@ -0,0 +1,1 @@
+export { type Section as _Section } from './Section.js';
