114dd50353a03e9758b1de9f7176a574e8e3cdf1
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2026-03-09 15:22:05 +0100

    fix: stylelint

--- src/components/form/components/input_groups/form_group.tsx
+++ src/components/form/components/input_groups/form_group.tsx
@@ -17,8 +17,7 @@ const FormContainer = styled.div<{
   margin: 0;
   grid-template-columns: ${(props) =>
     props.layout === 'inline' ? '[label] 30% [input] 70%' : '1fr'};
-  column-gap: 20px;
-  row-gap: 5px;
+  gap: 5px 20px;
   grid-template-rows: ${(props) =>
     props.layout === 'inline' ? 'auto auto' : 'auto'};
 `;
