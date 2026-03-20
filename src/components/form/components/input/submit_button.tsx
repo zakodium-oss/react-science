57a36ae425aa7749d071aa7471afce6f1dad94ea
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2026-03-20 15:56:01 +0100

    fix: allow disabling submit button (#995)

--- src/components/form/components/input/submit_button.tsx
+++ src/components/form/components/input/submit_button.tsx
@@ -7,7 +7,7 @@ import { useFormContext } from '../../context/use_ts_form.js';
 type SubmitButtonProps = ButtonProps;
 
 export function SubmitButton(props: SubmitButtonProps) {
-  const { intent, ...otherProps } = props;
+  const { intent, disabled, ...otherProps } = props;
 
   const form = useFormContext();
   const isSubmitting = useStore(form.store, (state) => state.isSubmitting);
@@ -17,7 +17,7 @@ export function SubmitButton(props: SubmitButtonProps) {
       {...otherProps}
       intent={intent ?? 'primary'}
       type="submit"
-      disabled={isSubmitting}
+      disabled={disabled || isSubmitting}
     />
   );
 }
