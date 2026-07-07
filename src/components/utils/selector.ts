3d08d1f3c079d34c8f907eca8b4d6c276485c8a3
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2026-07-07 11:20:15 +0200

    docs: add `selector`docstring and example

--- src/components/utils/selector.ts
+++ src/components/utils/selector.ts
@@ -1,3 +1,16 @@
+/**
+ * Escape string substitution for use in a CSS selector.
+ *
+ * @param template
+ * @param substitutions
+ *
+ * @example
+ * ```ts
+ * const element = scrollRef.current?.querySelector(
+ *   selector`tr[data-row-id="${id}"]`,
+ * );
+ * ```
+ */
 export function selector(
   template: TemplateStringsArray,
   ...substitutions: string[]
